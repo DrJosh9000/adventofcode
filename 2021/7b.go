45978f422a886f7c9b8b1af1c50c9b222661b49c
Josh Deprez <josh.deprez@gmail.com>
2021-12-07 16:07:18 +1100

    7

---
+++ 2021/7b.go
@@ -0,0 +1,54 @@
+package main
+
+import (
+	"fmt"
+	"log"
+	"math"
+	"os"
+	"strconv"
+	"strings"
+)
+
+func main() {
+	f, err := os.ReadFile("inputs/7.txt")
+	if err != nil {
+		log.Fatalf("Couldn't read: %v", err)
+	}
+
+	var crabs []int
+	min, max := math.MaxInt, math.MinInt
+	for _, s := range strings.Split(string(f), ",") {
+		n, err := strconv.Atoi(s)
+		if err != nil {
+			log.Fatalf("Couldn't atoi: %v", err)
+		}
+		crabs = append(crabs, n)
+		if n < min {
+			min = n
+		}
+		if n > max {
+			max = n
+		}
+	}
+
+	minfuel := math.MaxInt
+	for p := min; p <= max; p++ {
+		fuel := 0
+		for _, n := range crabs {
+			d := abs(p - n)
+			fuel += d * (d + 1) / 2
+		}
+		if fuel < minfuel {
+			minfuel = fuel
+		}
+	}
+
+	fmt.Println(minfuel)
+}
+
+func abs(x int) int {
+	if x < 0 {
+		return -x
+	}
+	return x
+}
