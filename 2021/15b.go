6b013a514662adb77487743e5ba158cf1277f6a0
Josh Deprez <josh.deprez@gmail.com>
2021-12-16 12:40:47 +1100

    15 again but with more priority queue and Dijkstra this time

--- 2021/15b.go
+++ 2021/15b.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"bufio"
+	"container/heap"
 	"fmt"
 	"image"
 	"log"
@@ -26,22 +27,23 @@ func main() {
 	}
 
 	// ok just make a copy 25 times larger then
-	cave := make([][]int, 5*len(sample))
+	w, h := 5*len(sample[0]), 5*len(sample)
+	cave := make([][]int, h)
 	for j := range cave {
-		cave[j] = make([]int, 5*len(sample[0]))
+		cave[j] = make([]int, w)
 	}
 	for j := range sample {
 		for i := range sample[j] {
 			cave[j][i] = int(sample[j][i] - '0')
 		}
-		for i := len(sample[j]); i < len(cave[j]); i++ {
+		for i := len(sample[j]); i < w; i++ {
 			cave[j][i] = cave[j][i-len(sample[j])] + 1
 			for cave[j][i] > 9 {
 				cave[j][i] -= 9
 			}
 		}
 	}
-	for j := len(sample); j < len(cave); j++ {
+	for j := len(sample); j < h; j++ {
 		for i := range cave[j] {
 			cave[j][i] = cave[j-len(sample)][i] + 1
 			for cave[j][i] > 9 {
@@ -50,48 +52,52 @@ func main() {
 		}
 	}
 
-	dist := make([][]int, len(cave))
+	dist := make([][]int, h)
 	for i := range dist {
-		dist[i] = make([]int, len(cave[0]))
+		dist[i] = make([]int, w)
 		for j := range dist[i] {
 			dist[i][j] = math.MaxInt
 		}
 	}
 	dist[0][0] = 0
 
-	queue := map[image.Point]struct{}{
-		{0, 0}: {},
-	}
-	for len(queue) > 0 {
-		for p := range queue {
-			delete(queue, p)
-			if p.X > 0 {
-				if t := dist[p.Y][p.X] + cave[p.Y][p.X-1]; t < dist[p.Y][p.X-1] {
-					dist[p.Y][p.X-1] = t
-					queue[image.Point{p.X - 1, p.Y}] = struct{}{}
-				}
+	visited := make(map[image.Point]struct{})
+	pq := &priqueue{item{p: image.Pt(0, 0), v: 0}}
+	for pq.Len() > 0 {
+		it := heap.Pop(pq).(item)
+		visited[it.p] = struct{}{}
+		for _, d := range []image.Point{{-1, 0}, {1, 0}, {0, -1}, {0, 1}} {
+			p := it.p.Add(d)
+			if p.X < 0 || p.X >= len(cave[0]) || p.Y < 0 || p.Y >= len(cave) {
+				continue
 			}
-			if p.X < len(cave[0])-1 {
-				if t := dist[p.Y][p.X] + cave[p.Y][p.X+1]; t < dist[p.Y][p.X+1] {
-					dist[p.Y][p.X+1] = t
-					queue[image.Point{p.X + 1, p.Y}] = struct{}{}
-				}
+			if _, no := visited[p]; no {
+				continue
 			}
-			if p.Y > 0 {
-				if t := dist[p.Y][p.X] + cave[p.Y-1][p.X]; t < dist[p.Y-1][p.X] {
-					dist[p.Y-1][p.X] = t
-					queue[image.Point{p.X, p.Y - 1}] = struct{}{}
-				}
+			if t := it.v + cave[p.Y][p.X]; t < dist[p.Y][p.X] {
+				dist[p.Y][p.X] = t
+				heap.Push(pq, item{p: p, v: t})
 			}
-			if p.Y < len(cave[0])-1 {
-				if t := dist[p.Y][p.X] + cave[p.Y+1][p.X]; t < dist[p.Y+1][p.X] {
-					dist[p.Y+1][p.X] = t
-					queue[image.Point{p.X, p.Y + 1}] = struct{}{}
-				}
-			}
-			break
 		}
 	}
 
-	fmt.Println(dist[len(dist)-1][len(dist[len(dist)-1])-1])
+	fmt.Println(dist[h-1][w-1])
+}
+
+type item struct {
+	p image.Point
+	v int
+}
+
+type priqueue []item
+
+func (pq priqueue) Len() int            { return len(pq) }
+func (pq priqueue) Less(i, j int) bool  { return pq[i].v < pq[j].v }
+func (pq priqueue) Swap(i, j int)       { pq[i], pq[j] = pq[j], pq[i] }
+func (pq *priqueue) Push(x interface{}) { *pq = append(*pq, x.(item)) }
+func (pq *priqueue) Pop() interface{} {
+	n1 := len(*pq) - 1
+	i := (*pq)[n1]
+	*pq = (*pq)[0:n1]
+	return i
 }
