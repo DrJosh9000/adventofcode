f564ce5b138c188b9ea891e3718c7216aba6009f
Josh Deprez <josh.deprez@gmail.com>
2021-12-24 20:56:45 +1100

    that's still gonna take a week

---
+++ 2021/24a-tp.go
@@ -0,0 +1,500 @@
+package main
+
+import (
+	"fmt"
+	"sync"
+	"sync/atomic"
+	"time"
+)
+
+var evals uint64
+	
+func main() {
+	fmt.Println("starting search")
+	start := time.Now()
+	go func() {
+		for range time.Tick(time.Minute) {
+			e := atomic.LoadUint64(&evals)
+			fmt.Println("evals/sec:", float64(e) / time.Since(start).Seconds())
+		}
+	}()
+	var wg sync.WaitGroup
+	for i := 9; i >= 1; i-- {
+		i := i
+		for j := 9; j >= 1; j-- {
+			j := j
+			wg.Add(1)
+			go func() {
+				search(append(make([]int, 0, 14), i, j))
+				wg.Done()
+			}()
+		}
+	}
+	wg.Wait()
+}
+
+func search(in []int) {
+	if len(in) == 14 {
+		if eval(in) {
+			fmt.Println(in)
+		}
+		return
+	}
+	for i := 9; i >= 1; i-- {
+		search(append(in, i))
+	}
+}
+
+func eval(in []int) bool {
+	w, x, y, z := 0, 0, 0, 0
+	w = in[0]
+	x *= 0
+	x += z
+	if x < 0 || 26 <= 0 {
+		return false
+	}
+	x %= 26
+	if 1 == 0 {
+		return false
+	}
+	z /= 1
+	x += 11
+	if x == w {
+		x = 1
+	} else { 
+		x = 0
+	}
+	if x == 0 {
+		x = 1
+	} else { 
+		x = 0
+	}
+	y *= 0
+	y += 25
+	y *= x
+	y += 1
+	z *= y
+	y *= 0
+	y += w
+	y += 14
+	y *= x
+	z += y
+	w = in[1]
+	x *= 0
+	x += z
+	if x < 0 || 26 <= 0 {
+		return false
+	}
+	x %= 26
+	if 1 == 0 {
+		return false
+	}
+	z /= 1
+	x += 13
+	if x == w {
+		x = 1
+	} else { 
+		x = 0
+	}
+	if x == 0 {
+		x = 1
+	} else { 
+		x = 0
+	}
+	y *= 0
+	y += 25
+	y *= x
+	y += 1
+	z *= y
+	y *= 0
+	y += w
+	y += 8
+	y *= x
+	z += y
+	w = in[2]
+	x *= 0
+	x += z
+	if x < 0 || 26 <= 0 {
+		return false
+	}
+	x %= 26
+	if 1 == 0 {
+		return false
+	}
+	z /= 1
+	x += 11
+	if x == w {
+		x = 1
+	} else { 
+		x = 0
+	}
+	if x == 0 {
+		x = 1
+	} else { 
+		x = 0
+	}
+	y *= 0
+	y += 25
+	y *= x
+	y += 1
+	z *= y
+	y *= 0
+	y += w
+	y += 4
+	y *= x
+	z += y
+	w = in[3]
+	x *= 0
+	x += z
+	if x < 0 || 26 <= 0 {
+		return false
+	}
+	x %= 26
+	if 1 == 0 {
+		return false
+	}
+	z /= 1
+	x += 10
+	if x == w {
+		x = 1
+	} else { 
+		x = 0
+	}
+	if x == 0 {
+		x = 1
+	} else { 
+		x = 0
+	}
+	y *= 0
+	y += 25
+	y *= x
+	y += 1
+	z *= y
+	y *= 0
+	y += w
+	y += 10
+	y *= x
+	z += y
+	w = in[4]
+	x *= 0
+	x += z
+	if x < 0 || 26 <= 0 {
+		return false
+	}
+	x %= 26
+	if 26 == 0 {
+		return false
+	}
+	z /= 26
+	x += -3
+	if x == w {
+		x = 1
+	} else { 
+		x = 0
+	}
+	if x == 0 {
+		x = 1
+	} else { 
+		x = 0
+	}
+	y *= 0
+	y += 25
+	y *= x
+	y += 1
+	z *= y
+	y *= 0
+	y += w
+	y += 14
+	y *= x
+	z += y
+	w = in[5]
+	x *= 0
+	x += z
+	if x < 0 || 26 <= 0 {
+		return false
+	}
+	x %= 26
+	if 26 == 0 {
+		return false
+	}
+	z /= 26
+	x += -4
+	if x == w {
+		x = 1
+	} else { 
+		x = 0
+	}
+	if x == 0 {
+		x = 1
+	} else { 
+		x = 0
+	}
+	y *= 0
+	y += 25
+	y *= x
+	y += 1
+	z *= y
+	y *= 0
+	y += w
+	y += 10
+	y *= x
+	z += y
+	w = in[6]
+	x *= 0
+	x += z
+	if x < 0 || 26 <= 0 {
+		return false
+	}
+	x %= 26
+	if 1 == 0 {
+		return false
+	}
+	z /= 1
+	x += 12
+	if x == w {
+		x = 1
+	} else { 
+		x = 0
+	}
+	if x == 0 {
+		x = 1
+	} else { 
+		x = 0
+	}
+	y *= 0
+	y += 25
+	y *= x
+	y += 1
+	z *= y
+	y *= 0
+	y += w
+	y += 4
+	y *= x
+	z += y
+	w = in[7]
+	x *= 0
+	x += z
+	if x < 0 || 26 <= 0 {
+		return false
+	}
+	x %= 26
+	if 26 == 0 {
+		return false
+	}
+	z /= 26
+	x += -8
+	if x == w {
+		x = 1
+	} else { 
+		x = 0
+	}
+	if x == 0 {
+		x = 1
+	} else { 
+		x = 0
+	}
+	y *= 0
+	y += 25
+	y *= x
+	y += 1
+	z *= y
+	y *= 0
+	y += w
+	y += 14
+	y *= x
+	z += y
+	w = in[8]
+	x *= 0
+	x += z
+	if x < 0 || 26 <= 0 {
+		return false
+	}
+	x %= 26
+	if 26 == 0 {
+		return false
+	}
+	z /= 26
+	x += -3
+	if x == w {
+		x = 1
+	} else { 
+		x = 0
+	}
+	if x == 0 {
+		x = 1
+	} else { 
+		x = 0
+	}
+	y *= 0
+	y += 25
+	y *= x
+	y += 1
+	z *= y
+	y *= 0
+	y += w
+	y += 1
+	y *= x
+	z += y
+	w = in[9]
+	x *= 0
+	x += z
+	if x < 0 || 26 <= 0 {
+		return false
+	}
+	x %= 26
+	if 26 == 0 {
+		return false
+	}
+	z /= 26
+	x += -12
+	if x == w {
+		x = 1
+	} else { 
+		x = 0
+	}
+	if x == 0 {
+		x = 1
+	} else { 
+		x = 0
+	}
+	y *= 0
+	y += 25
+	y *= x
+	y += 1
+	z *= y
+	y *= 0
+	y += w
+	y += 6
+	y *= x
+	z += y
+	w = in[10]
+	x *= 0
+	x += z
+	if x < 0 || 26 <= 0 {
+		return false
+	}
+	x %= 26
+	if 1 == 0 {
+		return false
+	}
+	z /= 1
+	x += 14
+	if x == w {
+		x = 1
+	} else { 
+		x = 0
+	}
+	if x == 0 {
+		x = 1
+	} else { 
+		x = 0
+	}
+	y *= 0
+	y += 25
+	y *= x
+	y += 1
+	z *= y
+	y *= 0
+	y += w
+	y += 0
+	y *= x
+	z += y
+	w = in[11]
+	x *= 0
+	x += z
+	if x < 0 || 26 <= 0 {
+		return false
+	}
+	x %= 26
+	if 26 == 0 {
+		return false
+	}
+	z /= 26
+	x += -6
+	if x == w {
+		x = 1
+	} else { 
+		x = 0
+	}
+	if x == 0 {
+		x = 1
+	} else { 
+		x = 0
+	}
+	y *= 0
+	y += 25
+	y *= x
+	y += 1
+	z *= y
+	y *= 0
+	y += w
+	y += 9
+	y *= x
+	z += y
+	w = in[12]
+	x *= 0
+	x += z
+	if x < 0 || 26 <= 0 {
+		return false
+	}
+	x %= 26
+	if 1 == 0 {
+		return false
+	}
+	z /= 1
+	x += 11
+	if x == w {
+		x = 1
+	} else { 
+		x = 0
+	}
+	if x == 0 {
+		x = 1
+	} else { 
+		x = 0
+	}
+	y *= 0
+	y += 25
+	y *= x
+	y += 1
+	z *= y
+	y *= 0
+	y += w
+	y += 13
+	y *= x
+	z += y
+	w = in[13]
+	x *= 0
+	x += z
+	if x < 0 || 26 <= 0 {
+		return false
+	}
+	x %= 26
+	if 26 == 0 {
+		return false
+	}
+	z /= 26
+	x += -12
+	if x == w {
+		x = 1
+	} else { 
+		x = 0
+	}
+	if x == 0 {
+		x = 1
+	} else { 
+		x = 0
+	}
+	y *= 0
+	y += 25
+	y *= x
+	y += 1
+	z *= y
+	y *= 0
+	y += w
+	y += 12
+	y *= x
+	z += y
+	atomic.AddUint64(&evals, 1)
+	return z == 0
+}
---
+++ 2021/24a.go
@@ -0,0 +1,131 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"log"
+	"os"
+	"os/exec"
+	"strings"
+)
+
+func main() {
+	f, err := os.Open("inputs/24.txt")
+	if err != nil {
+		log.Fatalf("Couldn't read input: %v", err)
+	}
+	defer f.Close()
+
+	tp, err := os.Create("24a-tp.go")
+	if err != nil {
+		log.Fatalf("Couldn't open intermediate: %v", err)
+	}
+	defer tp.Close()
+
+	_, err = tp.WriteString(`package main
+
+import (
+	"fmt"
+	"sync"
+	"sync/atomic"
+	"time"
+)
+
+var evals uint64
+	
+func main() {
+	fmt.Println("starting search")
+	start := time.Now()
+	go func() {
+		for range time.Tick(time.Minute) {
+			e := atomic.LoadUint64(&evals)
+			fmt.Println("evals/sec:", float64(e) / time.Since(start).Seconds())
+		}
+	}()
+	var wg sync.WaitGroup
+	for i := 9; i >= 1; i-- {
+		i := i
+		for j := 9; j >= 1; j-- {
+			j := j
+			wg.Add(1)
+			go func() {
+				search(append(make([]int, 0, 14), i, j))
+				wg.Done()
+			}()
+		}
+	}
+	wg.Wait()
+}
+
+func search(in []int) {
+	if len(in) == 14 {
+		if eval(in) {
+			fmt.Println(in)
+		}
+		return
+	}
+	for i := 9; i >= 1; i-- {
+		search(append(in, i))
+	}
+}
+
+func eval(in []int) bool {
+	w, x, y, z := 0, 0, 0, 0
+`)
+	if err != nil {
+		log.Fatalf("Couldn't write preamble: %v", err)
+	}
+
+	nextvar := 0
+	sc := bufio.NewScanner(f)
+	for sc.Scan() {
+		token := strings.Split(sc.Text(), " ")
+		if token[0] == "inp" {
+			fmt.Fprintf(tp, "\t%s = in[%d]\n", token[1], nextvar)
+			nextvar++
+			continue
+		}
+		switch token[0] {
+		case "add":
+			fmt.Fprintf(tp, "\t%s += %s\n", token[1], token[2])
+		case "mul":
+			fmt.Fprintf(tp, "\t%s *= %s\n", token[1], token[2])
+		case "div":
+			fmt.Fprintf(tp, `	if %s == 0 {
+		return false
+	}
+	%s /= %s
+`, token[2], token[1], token[2])
+		case "mod":
+			fmt.Fprintf(tp, `	if %s < 0 || %s <= 0 {
+		return false
+	}
+	%s %%= %s
+`, token[1], token[2], token[1], token[2])
+		case "eql":
+			fmt.Fprintf(tp, `	if %s == %s {
+		%s = 1
+	} else { 
+		%s = 0
+	}
+`, token[1], token[2], token[1], token[1])
+		}
+	}
+	if err := sc.Err(); err != nil {
+		log.Fatalf("Couldn't scan: %v", err)
+	}
+
+	tp.WriteString(`	atomic.AddUint64(&evals, 1)
+	return z == 0
+}
+`)
+	if err := tp.Close(); err != nil {
+		log.Fatalf("Couldn't close intermediate: %v", err)
+	}
+
+	cmd := exec.Command("go", "run", "24a-tp.go")
+	cmd.Stdout = os.Stdout
+	if err := cmd.Run(); err != nil {
+		log.Fatalf("Couldn't run intermediate: %v", err)
+	}
+}
