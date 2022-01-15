fb24bd3875a5f0b3135e7a550e461567cdb8286d
Josh Deprez <josh.deprez@gmail.com>
2022-01-15 13:51:21 +1100

    21 part 2 improvements

--- 2019/21b.go
+++ 2019/21b.go
@@ -16,7 +16,7 @@ import (
 var (
 	opcodes = []string{"AND", "OR", "NOT"}
 	rwregs  = []string{"T", "J"}
-	rregs   = []string{"A", "B", "C", "D", "E", "F", "G", "H", "I", "T", "J"}
+	rregs   = []string{"T", "J", "A", "B", "C", "D", "E", "F", "G", "H", "I"}
 
 	ovm *intcode.VM
 )
@@ -25,21 +25,32 @@ type inst struct {
 	opcode, in, out int
 }
 
+func randInst() inst {
+	// return an instruction that does something.
+	for {
+		i := inst{rand.Intn(3), rand.Intn(11), rand.Intn(2)}
+		if i.opcode == 2 || i.in != i.out {
+			return i
+		}
+	}
+}
+
 type prog struct {
-	i []inst
-	s int
+	i [15]inst
+	l int
 }
 
 func (p prog) String() string {
 	var b strings.Builder
-	for _, i := range p.i {
-		fmt.Fprintf(&b, "%s %s %s\n", opcodes[i.opcode], rregs[i.in], rwregs[i.out])
+	for i := 0; i < p.l; i++ {
+		in := p.i[i]
+		fmt.Fprintf(&b, "%s %s %s\n", opcodes[in.opcode], rregs[in.in], rwregs[in.out])
 	}
 	fmt.Fprintf(&b, "RUN\n\n")
 	return b.String()
 }
 
-func (p *prog) eval() {
+func (p *prog) eval() int {
 	input := p.String()
 	inpos := 0
 	vm := ovm.Copy()
@@ -58,88 +69,103 @@ vmLoop:
 			fmt.Println("Winner!")
 			fmt.Println("Program:")
 			fmt.Print(p)
-			fmt.Println("Cycles:", vm.CycleCount())
+			fmt.Println("Score:", vm.CycleCount())
 			fmt.Println("Damage:", x)
 			os.Exit(0)
-			return
+			break vmLoop
 		case in <- int(input[inpos]):
 			inpos++
 		}
 	}
-	p.s = vm.CycleCount()
+	return vm.CycleCount()
 }
 
 func (p *prog) mutate() {
-	ni := append([]inst(nil), p.i...)
-	n := len(ni)
-	switch rand.Intn(3) {
+	switch rand.Intn(4) {
 	case 0: // change one instruction
-		i := rand.Intn(n)
-		ni[i] = inst{rand.Intn(3), rand.Intn(11), rand.Intn(2)}
+		i := rand.Intn(p.l)
+		p.i[i] = randInst()
 	case 1: // insert an instruction
-		if n >= 15 {
+		if p.l >= 15 {
 			break
 		}
-		ni = append(ni, inst{rand.Intn(3), rand.Intn(11), rand.Intn(2)})
-		i := rand.Intn(n)
-		ni[i], ni[n] = ni[n], ni[i]
+		p.i[p.l] = randInst()
+		i := rand.Intn(p.l)
+		p.i[i], p.i[p.l] = p.i[p.l], p.i[i]
+		p.l++
 	case 2: // delete an instruction
-		if n <= 1 {
+		if p.l <= 1 {
 			break
 		}
-		i := rand.Intn(n)
-		ni = append(ni[:i], ni[i+1:]...)
+		i := rand.Intn(p.l)
+		for j := i; j < p.l-1; j++ {
+			p.i[j] = p.i[j+1]
+		}
+		p.l--
+		p.i[p.l] = inst{}
 	case 3: // swap two instructions
-		i, j := rand.Intn(n), rand.Intn(n)
-		ni[i], ni[j] = ni[j], ni[i]
+		i, j := rand.Intn(p.l), rand.Intn(p.l)
+		p.i[i], p.i[j] = p.i[j], p.i[i]
 	}
-	p.i = ni
 }
 
 func main() {
 	ovm = intcode.ReadProgram("inputs/21.txt")
 
-	const N = 1000
+	const N = 500
 
-	pool := make([]prog, 0, N)
-	for i := 0; i < N; i++ {
+	type progscore struct {
+		p prog
+		s int
+	}
+	var ranked []progscore
+	pool := make(map[prog]int)
+	for i := 0; len(pool) < N; i++ {
 		var p prog
-		for j := 0; j < (i%14)+1; j++ {
-			p.i = append(p.i, inst{rand.Intn(3), rand.Intn(11), rand.Intn(2)})
+		p.l = (i % 14) + 1
+		for j := 0; j < p.l; j++ {
+			p.i[j] = randInst()
 		}
-		p.eval()
-		pool = append(pool, p)
+		if _, exists := pool[p]; exists {
+			continue
+		}
+		s := p.eval()
+		pool[p] = s
+		ranked = append(ranked, progscore{p, s})
 	}
 
 	itcount := 0
 	for {
-		sort.Slice(pool, func(i, j int) bool {
-			return pool[i].s > pool[j].s
+		sort.Slice(ranked, func(i, j int) bool {
+			return ranked[i].s > ranked[j].s
 		})
-		if itcount%10 == 0 {
-			log.Printf("%d iterations: best score %d", itcount, pool[0].s)
-		}
-		pool = pool[:N/2]
+		log.Printf("%d iterations: best score %d", itcount, ranked[0].s)
 
-		var mu sync.Mutex
+		var mu sync.RWMutex
 		var wg sync.WaitGroup
 		wg.Add(runtime.NumCPU())
-		work := make(chan int)
+		work := make(chan prog)
 		for i := 0; i < runtime.NumCPU(); i++ {
 			go func() {
-				for j := range work {
-					p := pool[j]
-					p.mutate()
-					p.eval()
+				for p := range work {
+					known := true
+					for known {
+						p.mutate()
+						mu.RLock()
+						_, known = pool[p]
+						mu.RUnlock()
+					}
+					s := p.eval()
 					mu.Lock()
-					pool = append(pool, p)
+					pool[p] = s
+					ranked = append(ranked, progscore{p, s})
 					mu.Unlock()
 				}
 				wg.Done()
 			}()
 		}
-		for i := 0; i < N/2; i++ {
-			work <- i
+		for _, ps := range ranked[:N] {
+			work <- ps.p
 		}
 		close(work)
 		wg.Wait()
