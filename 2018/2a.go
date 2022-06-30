e79c4133176327fa917a4687454c4de7b96dbe8a
Josh Deprez <josh.deprez@gmail.com>
2022-06-30 02:54:23 +0000

    2018/2

---
+++ 2018/2a.go
@@ -0,0 +1,25 @@
+package main
+
+import (
+	"fmt"
+
+	"github.com/DrJosh9000/exp"
+)
+
+func main() {
+	two, three := 0, 0
+	exp.MustForEachLineIn("inputs/2.txt", func(line string) {
+		m := make(map[rune]int)
+		for _, r := range line {
+			m[r]++
+		}
+		c2, c3 := false, false
+		for _, v := range m {
+			c2 = c2 || (v == 2)
+			c3 = c3 || (v == 3)
+		}
+		if c2 { two++ }
+		if c3 { three++ }
+	})
+	fmt.Println(two * three)
+}
---
+++ 2018/2b.go
@@ -0,0 +1,40 @@
+package main
+
+import (
+	"fmt"
+
+	"github.com/DrJosh9000/exp"
+)
+
+func main() {
+	ids := []string{}
+	exp.MustForEachLineIn("inputs/2.txt", func(line string) {
+		ids = append(ids, line)
+	})
+	for i, a := range ids[:len(ids)-1] {
+	idsLoop:
+		for _, b := range ids[i+1:] {
+			d := 0
+			p := -1
+			for j := range a {
+				if a[j] != b[j] {
+					d++
+					if d > 1 {
+						continue idsLoop
+					}
+					p = j
+				}
+			}
+			if d == 1 {
+				for j, r := range a {
+					if j == p {
+						continue
+					}
+					fmt.Printf("%c", r)
+				}
+				fmt.Println()
+				return
+			}
+		}
+	}
+}
