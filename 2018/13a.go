46fd252e10c674b8215b0db426c0e938d626c251
Josh Deprez <josh.deprez@gmail.com>
2022-07-13 03:09:23 +0000

    2018/13a

---
+++ 2018/13a.go
@@ -0,0 +1,143 @@
+package main
+
+import (
+	"fmt"
+	"image"
+	"sort"
+
+	"github.com/DrJosh9000/exp"
+)
+
+type dirtrack struct {
+	d, t rune
+}
+
+var dirs = map[rune]image.Point{
+	'>': {1, 0},
+	'<': {-1, 0},
+	'^': {0, -1},
+	'v': {0, 1},
+}
+
+var nextDir = map[dirtrack]rune {
+	// existing carts = straight track
+	{'>', '>'}: '>',
+	{'<', '<'}: '<',
+	{'^', '^'}: '^',
+	{'v', 'v'}: 'v',
+	{'>', '<'}: '>',
+	{'<', '>'}: '<',
+	{'^', 'v'}: '^',
+	{'v', '^'}: 'v',
+	// straight track
+	{'>', '-'}: '>',
+	{'<', '-'}: '<',
+	{'^', '|'}: '^',
+	{'v', '|'}: 'v',
+	// crossings
+	{'>', '+'}: '>',
+	{'<', '+'}: '<',
+	{'^', '+'}: '^',
+	{'v', '+'}: 'v',
+	// turns
+	{'>', '\\'}: 'v',
+	{'>', '/'}: '^',
+	{'<', '\\'}: '^',
+	{'<', '/'}: 'v',
+	{'^', '\\'}: '<',
+	{'^', '/'}: '>',
+	{'v', '\\'}: '>',
+	{'v', '/'}: '<',
+}
+
+type bias int 
+
+const (
+	left = bias(iota)
+	straight
+	right
+	numBiases
+)
+
+type dirbias struct {
+	d rune
+	b bias
+}
+
+var crossings = map[dirbias]rune {
+	{'>', left}: '^',
+	{'>', straight}: '>',
+	{'>', right}: 'v',
+	{'<', left}: 'v',
+	{'<', straight}: '<',
+	{'<', right}: '^',
+	{'^', left}: '<',
+	{'^', straight}: '^',
+	{'^', right}: '>',
+	{'v', left}: '>',
+	{'v', straight}: 'v',
+	{'v', right}: '<',
+}
+
+type cart struct {
+	image.Point
+	d rune
+	b bias
+}
+
+func isCart(x rune) bool {
+	return x == '>' || x == '<' || x == '^' || x == 'v'
+}
+
+func sortCarts(carts []cart) {
+	sort.Slice(carts, func(i, j int) bool {
+		if carts[i].Y == carts[j].Y {
+			return carts[i].X < carts[j].X
+		}
+		return carts[i].Y < carts[j].Y
+	})
+}
+
+func main() {
+	var grid []string
+	var carts []cart
+	r := 0
+	exp.MustForEachLineIn("inputs/13.txt", func(line string) {
+		grid = append(grid, line)
+		for c, x := range line {
+			if isCart(x) {
+				carts = append(carts, cart{
+					Point: image.Pt(c, r),
+					d: x,
+				})
+			}
+		}
+		r++
+	})
+	
+	for tick := 0; ; tick++ {
+		sortCarts(carts)
+		for i, c := range carts {
+			p := c.Add(dirs[c.d])
+			for j, d := range carts {
+				if j == i {
+					continue
+				}
+				if p == d.Point {
+					fmt.Println(p)
+					return
+				}
+			}
+			c.Point = p
+			t := rune(grid[p.Y][p.X])
+			if t == '+' {
+				c.d = crossings[dirbias{c.d, c.b}]
+				c.b++
+				c.b %= numBiases
+			} else {
+				c.d = nextDir[dirtrack{c.d, t}]
+			}
+			carts[i] = c
+		}
+	}
+}
