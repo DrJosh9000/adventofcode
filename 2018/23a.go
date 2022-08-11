27a9f9104a241d660cd81cbe3dd4620e30b30c79
Josh Deprez <josh.deprez@gmail.com>
2022-08-11 02:54:34 +0000

    2018/23a

---
+++ 2018/23a.go
@@ -0,0 +1,46 @@
+package main
+
+import (
+	"fmt"
+	"log"
+	"github.com/DrJosh9000/exp"
+)
+
+type nanobot struct {
+	x, y, z, r int
+}
+
+func abs(x int) int {
+	if x < 0 {
+		return -x
+	}
+	return x
+}
+
+func dist(v, w nanobot) int {
+	return abs(v.x - w.x) + abs(v.y - w.y) + abs(v.z - w.z)
+}
+
+func main() {
+	var bots []nanobot
+	maxr, best := -1, -1
+	exp.MustForEachLineIn("inputs/23.txt", func(line string) {
+		var bot nanobot
+		if _, err := fmt.Sscanf(line, "pos=<%d,%d,%d>, r=%d", &bot.x, &bot.y, &bot.z, &bot.r); err != nil {
+			log.Fatalf("Couldn't scan line: %v", err)
+		}
+		if bot.r > maxr {
+			maxr = bot.r
+			best = len(bots)
+		}
+		bots = append(bots, bot)
+	})
+	
+	count := 0
+	for _, b := range bots {
+		if dist(b, bots[best]) <= maxr {
+			count++
+		}
+	}
+	fmt.Println(count)
+}
