9c9522f5dcc7d9c503d7cca6c17e6c33597af030
Josh Deprez <josh.deprez@gmail.com>
2022-08-31 10:33:36 +1000

    2017/15

---
+++ 2017/15a.go
@@ -0,0 +1,31 @@
+package main
+
+import (
+	"fmt"
+
+	"github.com/DrJosh9000/exp"
+)
+
+func main() {
+	const mod = 2147483647
+	const mask = 0xffff
+	factor := [2]int{16807, 48271}
+	var gen [2]int
+	exp.MustForEachLineIn("inputs/15.txt", func(line string) {
+		var s int
+		var g rune
+		exp.Must(fmt.Sscanf(line, "Generator %c starts with %d", &g, &s))
+		gen[g-'A'] = s
+	})
+	
+	judge := 0
+	for i := 0; i < 40_000_000; i++ {
+		for j := range gen {
+			gen[j] = (gen[j] * factor[j]) % mod
+		}
+		if gen[0] & mask == gen[1] & mask {
+			judge++
+		}
+	}
+	fmt.Println(judge)
+}
---
+++ 2017/15b.go
@@ -0,0 +1,40 @@
+package main
+
+import (
+	"fmt"
+
+	"github.com/DrJosh9000/exp"
+)
+
+func main() {
+	const mod = 2147483647
+	const mask = 0xffff
+	factor := [2]int{16807, 48271}
+	div := [2]int{4, 8}
+	gch := [2]chan int{ make(chan int), make(chan int) }
+	gen := func(x, f, d int, ch chan int) {
+		for {
+			x = (x * f) % mod
+			if x % d == 0 {
+				ch <- x
+			}
+		}
+	}
+	exp.MustForEachLineIn("inputs/15.txt", func(line string) {
+		var s int
+		var g rune
+		exp.Must(fmt.Sscanf(line, "Generator %c starts with %d", &g, &s))
+		n := g - 'A'
+		go gen(s, factor[n], div[n], gch[n])
+	})
+	
+	
+	judge := 0
+	for i := 0; i < 5_000_000; i++ {
+		a, b := <-gch[0], <-gch[1]
+		if a & mask == b & mask {
+			judge++
+		}
+	}
+	fmt.Println(judge)
+}
