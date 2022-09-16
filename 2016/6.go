3a0c53561e5df46c11f77c2ca8aa400a08dce1f5
Josh Deprez <josh.deprez@gmail.com>
2022-09-16 20:44:13 +1000

    2016/6

---
+++ 2016/6.go
@@ -0,0 +1,38 @@
+package main
+
+import (
+	"fmt"
+
+	"github.com/DrJosh9000/exp"
+	"github.com/DrJosh9000/exp/algo"
+)
+
+// Advent of Code 2016
+// Day 6
+
+func main() {
+	var comm []map[rune]int
+	for _, line := range exp.MustReadLines("inputs/6.txt") {
+		if len(comm) == 0 {
+			comm = make([]map[rune]int, len(line))
+			for i := range comm {
+				comm[i] = make(map[rune]int)
+			}
+		}
+		for i, c := range line {
+			comm[i][c]++
+		}
+	}
+
+	for _, h := range comm {
+		c, _ := algo.MapMax(h)
+		fmt.Printf("%c", c)
+	}
+	fmt.Println()
+
+	for _, h := range comm {
+		c, _ := algo.MapMin(h)
+		fmt.Printf("%c", c)
+	}
+	fmt.Println()
+}
