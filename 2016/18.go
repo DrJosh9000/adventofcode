65a862e8f4217f2ad2b7a1cd324c7f37203ad6e7
Josh Deprez <josh.deprez@gmail.com>
2022-10-05 17:10:29 +1100

    2016/18: fix bug introduced at last minute

--- 2016/18.go
+++ 2016/18.go
@@ -6,30 +6,22 @@ import (
 	"os"
 
 	"github.com/DrJosh9000/exp"
+	"github.com/DrJosh9000/exp/algo"
 )
 
 // Advent of Code 2016
 // Day 18
 
-func evolve(dst, src []byte) int {
-	safe := len(src)
+func evolve(dst, src []byte) {
 	n1 := len(src) - 1
 	dst[0] = src[1]
-	if dst[0] == '^' {
-		safe--
-	}
 	dst[n1] = src[n1-1]
-	if dst[n1] == '^' {
-		safe--
-	}
 	for i := 1; i < n1; i++ {
 		dst[i] = '.'
 		if src[i-1] != src[i+1] {
 			dst[i] = '^'
-			safe--
 		}
 	}
-	return safe
 }
 
 func main() {
@@ -38,12 +30,14 @@ func main() {
 	sum := 0
 	i := 0
 	for ; i < 40; i++ {
-		sum += evolve(l2, l1)
+		sum += algo.Count(l1, '.')
+		evolve(l2, l1)
 		l1, l2 = l2, l1
 	}
 	fmt.Println(sum)
 	for ; i < 400_000; i++ {
-		sum += evolve(l2, l1)
+		sum += algo.Count(l1, '.')
+		evolve(l2, l1)
 		l1, l2 = l2, l1
 	}
 	fmt.Println(sum)
--- go.mod
+++ go.mod
@@ -3,7 +3,7 @@ module github.com/DrJosh9000/adventofcode
 go 1.19
 
 require (
-	github.com/DrJosh9000/exp v0.0.0-20220929033343-41213f90e869
+	github.com/DrJosh9000/exp v0.0.0-20221005060856-2d5c55b21d05
 	github.com/bearmini/bitstream-go v0.0.0-20190121230027-bec1c9ea0d3c
 	golang.org/x/exp v0.0.0-20220916125017-b168a2c6b86b
 	gonum.org/v1/plot v0.10.0
