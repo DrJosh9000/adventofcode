1e90b2ba8f23448361dcf9baae69404128d4707e
Josh Deprez <josh.deprez@gmail.com>
2022-10-06 11:47:28 +1100

    2016/19

---
+++ 2016/19a.go
@@ -0,0 +1,29 @@
+package main
+
+import (
+	"fmt"
+	"os"
+	"strconv"
+
+	"github.com/DrJosh9000/exp"
+)
+
+// Advent of Code 2016
+// Day 19, part a
+
+func main() {
+	n := exp.Must(strconv.Atoi(os.Args[1]))
+
+	e := make([]int, n)
+	for i := range e[1:] {
+		e[i] = i + 1
+	}
+	e[n-1] = 0
+
+	p := 0
+	for ; e[p] != p; p = e[p] {
+		e[p] = e[e[p]]
+	}
+
+	fmt.Println(p + 1)
+}
---
+++ 2016/19b.go
@@ -0,0 +1,36 @@
+package main
+
+import (
+	"fmt"
+	"os"
+	"strconv"
+
+	"github.com/DrJosh9000/exp"
+)
+
+// Advent of Code 2016
+// Day 19, part b
+
+func main() {
+	n := exp.Must(strconv.Atoi(os.Args[1]))
+
+	// Solution: generate a run of answers for small inputs with a full
+	// simulation, and assume the pattern holds for all inputs.
+
+	p, step := 0, 1
+	for i := 1; ; i++ {
+		p += step
+		if i == n {
+			fmt.Println(p)
+			return
+		}
+		switch i {
+		case p:
+			step = 1
+			p = 0
+		case p * 2:
+			step = 2
+		}
+	}
+
+}
