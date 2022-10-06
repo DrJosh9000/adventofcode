a460bd0a68158e7be189b78602fa20352e4b7f13
Josh Deprez <josh.deprez@gmail.com>
2022-10-06 12:04:46 +1100

    2016/20

---
+++ 2016/20.go
@@ -0,0 +1,49 @@
+package main
+
+import (
+	"fmt"
+
+	"github.com/DrJosh9000/exp"
+	"github.com/DrJosh9000/exp/algo"
+)
+
+// Advent of Code 2016
+// Day 20
+
+func main() {
+	type rainj struct { // "range" is a keyword
+		a, b uint
+	}
+
+	allowed := algo.Set[rainj]{
+		{0, 0xffffffff}: {},
+	}
+	for _, line := range exp.MustReadLines("inputs/20.txt") {
+		var r rainj
+		exp.Must(fmt.Sscanf(line, "%d-%d", &r.a, &r.b))
+
+		for a := range allowed {
+			if a.a > r.b || a.b < r.a {
+				continue
+			}
+			delete(allowed, a)
+			if r.a > a.a {
+				allowed.Insert(rainj{a.a, r.a - 1})
+			}
+			if r.b < a.b {
+				allowed.Insert(rainj{r.b + 1, a.b})
+			}
+		}
+	}
+
+	min := uint(0xffffffff)
+	sum := uint(0)
+	for a := range allowed {
+		if a.a < min {
+			min = a.a
+		}
+		sum += a.b - a.a + 1
+	}
+	fmt.Println(min)
+	fmt.Println(sum)
+}
