12a31cfe2c5e8592c0d253dc4897fa1144eb6551
Josh Deprez <josh.deprez@gmail.com>
2022-10-07 15:02:02 +1100

    2016/22

---
+++ 2016/22a.go
@@ -0,0 +1,45 @@
+package main
+
+import (
+	"fmt"
+	"image"
+	"strings"
+
+	"github.com/DrJosh9000/exp"
+)
+
+// Advent of Code 2016
+// Day 22, part a
+
+func main() {
+	type node struct {
+		size, used, avail, usep int
+	}
+	nodes := make(map[image.Point]node)
+	for _, line := range exp.MustReadLines("inputs/22.txt") {
+		if !strings.HasPrefix(line, "/dev") {
+			continue
+		}
+		var n node
+		var p image.Point
+		exp.Must(fmt.Sscanf(line, "/dev/grid/node-x%d-y%d %dT %dT %dT %d%%", &p.X, &p.Y, &n.size, &n.used, &n.avail, &n.usep))
+		nodes[p] = n
+	}
+
+	viable := 0
+	for p, n := range nodes {
+		if n.used == 0 {
+			continue
+		}
+		for q, m := range nodes {
+			if p == q {
+				continue
+			}
+			if n.used <= m.avail {
+				//fmt.Printf("can move %dT\tfrom %v\tto %v\n", n.used, p, q)
+				viable++
+			}
+		}
+	}
+	fmt.Println(viable)
+}
---
+++ 2016/22b.go
@@ -0,0 +1,70 @@
+package main
+
+import (
+	"errors"
+	"fmt"
+	"image"
+	"strings"
+
+	"github.com/DrJosh9000/exp"
+	"github.com/DrJosh9000/exp/algo"
+)
+
+// Advent of Code 2016
+// Day 22, part b
+
+func main() {
+	// The assumptions given in the part 2 description seem to hold:
+	// There is indeed only one empty node, that can handle small but not large
+	// amounts of data.
+	// So we only need to track where the hole is, and where the goal data is.
+	// (A more general solution is possible, it merely isn't necessary.)
+	type state struct {
+		hole, goal image.Point
+	}
+	var start state
+	lim := 0
+
+	type node struct {
+		size, used, avail, usep int
+	}
+	nodes := make(map[image.Point]node)
+	for _, line := range exp.MustReadLines("inputs/22.txt") {
+		if !strings.HasPrefix(line, "/dev") {
+			continue
+		}
+		var n node
+		var p image.Point
+		exp.Must(fmt.Sscanf(line, "/dev/grid/node-x%d-y%d %dT %dT %dT %d%%", &p.X, &p.Y, &n.size, &n.used, &n.avail, &n.usep))
+		nodes[p] = n
+
+		if n.used == 0 {
+			start.hole = p
+			lim = n.avail
+		}
+		if p.Y == 0 && p.X > start.goal.X {
+			start.goal = p
+		}
+	}
+
+	algo.FloodFill(start, func(s state, d int) ([]state, error) {
+		if s.goal == (image.Point{}) {
+			fmt.Println(d)
+			return nil, errors.New("all done")
+		}
+		var out []state
+		for _, Δ := range algo.Neigh4 {
+			p := s.hole.Add(Δ)
+			if n, ok := nodes[p]; !ok || n.used > lim {
+				// doesn't exist or too big
+				continue
+			}
+			if p == s.goal {
+				out = append(out, state{hole: s.goal, goal: s.hole})
+			} else {
+				out = append(out, state{hole: p, goal: s.goal})
+			}
+		}
+		return out, nil
+	})
+}
