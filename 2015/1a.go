854d6a56bdcd279639399a754eb966731791ad09
Josh Deprez <josh.deprez@gmail.com>
2022-10-13 11:45:15 +1100

    2015/1

---
+++ 2015/1a.go
@@ -0,0 +1,17 @@
+package main
+
+import (
+	"fmt"
+	"os"
+
+	"github.com/DrJosh9000/exp"
+	"github.com/DrJosh9000/exp/algo"
+)
+
+// Advent of Code 2015
+// Day 1, part a
+
+func main() {
+	h := algo.Freq(exp.Must(os.ReadFile("inputs/1.txt")))
+	fmt.Println(h['('] - h[')'])
+}
---
+++ 2015/1b.go
@@ -0,0 +1,27 @@
+package main
+
+import (
+	"fmt"
+	"os"
+
+	"github.com/DrJosh9000/exp"
+)
+
+// Advent of Code 2015
+// Day 1, part b
+
+func main() {
+	f := 0
+	for i, c := range exp.Must(os.ReadFile("inputs/1.txt")) {
+		switch c {
+		case '(':
+			f++
+		case ')':
+			f--
+		}
+		if f == -1 {
+			fmt.Println(i + 1)
+			return
+		}
+	}
+}
