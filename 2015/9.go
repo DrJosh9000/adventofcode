c5ae5534bbc42c658c7062b701ce34660e8e09a1
Josh Deprez <josh.deprez@gmail.com>
2022-11-04 10:51:36 +1100

    2015/9

---
+++ 2015/9.go
@@ -0,0 +1,72 @@
+package main
+
+import (
+	"fmt"
+	"math"
+	"strings"
+
+	"github.com/DrJosh9000/exp"
+	"github.com/DrJosh9000/exp/algo"
+	"github.com/DrJosh9000/exp/grid"
+)
+
+// Advent of Code 2015
+// Day 9
+
+// Travelling Salesman let's goooooo
+
+func main() {
+	label := make(map[string]int)
+	input := exp.MustReadLines("inputs/9.txt")
+	for _, line := range input {
+		fs := strings.Fields(line)
+		if _, l := label[fs[0]]; !l {
+			label[fs[0]] = len(label)
+		}
+		if _, l := label[fs[2]]; !l {
+			label[fs[2]] = len(label)
+		}
+	}
+
+	N := len(label)
+
+	dist := grid.Make[int](N, N)
+	for _, line := range input {
+		var s, t string
+		var d int
+		exp.Must(fmt.Sscanf(line, "%s to %s = %d", &s, &t, &d))
+		x, y := label[s], label[t]
+		if x == y {
+			fmt.Printf("that's weird: %q and %q label the same node %d\n", s, t, x)
+		}
+		dist[x][y] = d
+		dist[y][x] = d
+	}
+
+	fmt.Printf("distance matrix:\n%v\n", dist)
+
+	ord := make([]int, N)
+	for i := range ord {
+		ord[i] = i
+	}
+
+	mind, maxd := math.MaxInt, math.MinInt
+	for {
+		d := 0
+		p := ord[0]
+		for _, q := range ord[1:] {
+			d += dist[p][q]
+			p = q
+		}
+		if d < mind {
+			mind = d
+		}
+		if d > maxd {
+			maxd = d
+		}
+		if !algo.NextPermutation(ord) {
+			break
+		}
+	}
+	fmt.Println(mind, maxd)
+}
