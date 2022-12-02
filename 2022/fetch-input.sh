583915f8b652180ef17fdc761865d7968a3d8286
Josh Deprez <josh.deprez@gmail.com>
2022-12-02 16:29:30 +1100

    fetch-input improvements

--- 2022/fetch-input.sh
+++ 2022/fetch-input.sh
@@ -1,6 +1,10 @@
+#!/bin/bash
 
-day=$(date +%-d)
+set -eu
 
+day=${1:-$(date +%-d)}
+
+echo "Fetching inputs/${day}.txt..."
 curl --cookie "session=${AOC_SESSION}" \
   -o "inputs/${day}.txt" \
   "https://adventofcode.com/2022/day/${day}/input"
