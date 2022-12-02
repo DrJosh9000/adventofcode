b925eacd547eb1d325947b59758f2056072adeec
Josh Deprez <josh.deprez@gmail.com>
2022-12-02 16:14:28 +1100

    2022/2

--- 2022/2a.go
+++ 2022/2a.go
@@ -2,7 +2,7 @@ package main
 
 import (
 	"fmt"
-	"strconv"
+	"strings"
 
 	"github.com/DrJosh9000/exp"
 )
@@ -10,10 +10,30 @@ import (
 // Advent of Code 2022
 // Day 2, part a
 
+var shape = map[string]int{
+	"X": 1,
+	"Y": 2,
+	"Z": 3,
+}
+
+var outcome = map[string]int{
+	"A X": 3, // rock vs rock
+	"A Y": 6, // rock vs paper
+	"A Z": 0, // rock vs scissors
+	"B X": 0, // paper vs rock
+	"B Y": 3, // paper vs paper
+	"B Z": 6, // paper vs scissors
+	"C X": 6, // scissors vs rock
+	"C Y": 0, // scissors vs paper
+	"C Z": 3, // scissors vs scissors
+}
+
 func main() {
-	sum := 0
+	score := 0
 	for _, line := range exp.MustReadLines("inputs/2.txt") {
-		sum += exp.Must(strconv.Atoi(line))
+		m := strings.Fields(line)
+		score += outcome[line]
+		score += shape[m[1]]
 	}
-	fmt.Println(sum)
+	fmt.Println(score)
 }
--- 2022/2b.go
+++ 2022/2b.go
@@ -2,7 +2,7 @@ package main
 
 import (
 	"fmt"
-	"strconv"
+	"strings"
 
 	"github.com/DrJosh9000/exp"
 )
@@ -10,10 +10,43 @@ import (
 // Advent of Code 2022
 // Day 2, part b
 
+var shape = map[string]int{
+	"A": 1,
+	"B": 2,
+	"C": 3,
+}
+
+var choice = map[string]string{
+	"A X": "C", // rock lose = scissors
+	"A Y": "A", // rock draw = rock
+	"A Z": "B", // rock win = paper
+	"B X": "A", // paper lose = rock
+	"B Y": "B", // paper draw = paper
+	"B Z": "C", // paper win = scissors
+	"C X": "B", // scissors lose = paper
+	"C Y": "C", // scissors draw = scissors
+	"C Z": "A", // scissors win = rock
+}
+
+var outcome = map[string]int{
+	"A A": 3, // rock vs rock
+	"A B": 6, // rock vs paper
+	"A C": 0, // rock vs scissors
+	"B A": 0, // paper vs rock
+	"B B": 3, // paper vs paper
+	"B C": 6, // paper vs scissors
+	"C A": 6, // scissors vs rock
+	"C B": 0, // scissors vs paper
+	"C C": 3, // scissors vs scissors
+}
+
 func main() {
-	sum := 0
+	score := 0
 	for _, line := range exp.MustReadLines("inputs/2.txt") {
-		sum += exp.Must(strconv.Atoi(line))
+		m := strings.Fields(line)
+		c := choice[line]
+		score += outcome[m[0]+" "+c]
+		score += shape[c]
 	}
-	fmt.Println(sum)
+	fmt.Println(score)
 }
