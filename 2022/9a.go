dba7cd9fba875f5fa2e85050ff86ba14ddaa86a2
Josh Deprez <josh.deprez@gmail.com>
2022-12-09 16:14:01 +1100

    2022/9

--- 2022/9a.go
+++ 2022/9a.go
@@ -2,18 +2,57 @@ package main
 
 import (
 	"fmt"
-	"strconv"
+	"image"
 
 	"github.com/DrJosh9000/exp"
+	"github.com/DrJosh9000/exp/algo"
 )
 
 // Advent of Code 2022
 // Day 9, part a
 
+func norm(p image.Point) int {
+	return algo.Max(algo.Abs(p.X), algo.Abs(p.Y))
+}
+
 func main() {
-	sum := 0
+	visited := make(algo.Set[image.Point])
+	var h, t image.Point
 	for _, line := range exp.MustReadLines("inputs/9.txt") {
-		sum += exp.Must(strconv.Atoi(line))
+		var d rune
+		var s int
+		exp.Must(fmt.Sscanf(line, "%c %d", &d, &s))
+		for i := 0; i < s; i++ {
+			switch d {
+			case 'R':
+				h = h.Add(image.Pt(1, 0))
+			case 'L':
+				h = h.Add(image.Pt(-1, 0))
+			case 'U':
+				h = h.Add(image.Pt(0, -1))
+			case 'D':
+				h = h.Add(image.Pt(0, 1))
+			}
+			delta := h.Sub(t)
+			if norm(delta) > 1 {
+				step := delta
+				if delta.X < -1 {
+					step.X = -1
+				}
+				if delta.X > 1 {
+					step.X = 1
+				}
+				if delta.Y < -1 {
+					step.Y = -1
+				}
+				if delta.Y > 1 {
+					step.Y = 1
+				}
+				t = t.Add(step)
+			}
+
+			visited.Insert(t)
+		}
 	}
-	fmt.Println(sum)
+	fmt.Println(len(visited))
 }
