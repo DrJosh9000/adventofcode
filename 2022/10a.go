54e9f0398278bedca637102a62f711ae8a91cb05
Josh Deprez <josh.deprez@gmail.com>
2022-12-10 16:22:07 +1100

    2022/10

--- 2022/10a.go
+++ 2022/10a.go
@@ -3,17 +3,35 @@ package main
 import (
 	"fmt"
 	"strconv"
+	"strings"
 
 	"github.com/DrJosh9000/exp"
+	"github.com/DrJosh9000/exp/algo"
 )
 
 // Advent of Code 2022
 // Day 10, part a
 
 func main() {
+	x := 1
+	c := 1
 	sum := 0
+	want := algo.Set[int]{20: {}, 60: {}, 100: {}, 140: {}, 180: {}, 220: {}}
 	for _, line := range exp.MustReadLines("inputs/10.txt") {
-		sum += exp.Must(strconv.Atoi(line))
+		fs := strings.Fields(line)
+		if want.Contains(c) {
+			sum += x * c
+		}
+		switch fs[0] {
+		case "addx":
+			if want.Contains(c + 1) {
+				sum += x * (c + 1)
+			}
+			c += 2
+			x += exp.Must(strconv.Atoi(fs[1]))
+		case "noop":
+			c++
+		}
 	}
 	fmt.Println(sum)
 }
