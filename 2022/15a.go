98a58b81570f7700bbd519c1b371970e55455786
Josh Deprez <josh.deprez@gmail.com>
2022-12-15 16:27:44 +1100

    2022/15

--- 2022/15a.go
+++ 2022/15a.go
@@ -2,18 +2,50 @@ package main
 
 import (
 	"fmt"
-	"strconv"
+	"image"
 
 	"github.com/DrJosh9000/exp"
+	"github.com/DrJosh9000/exp/algo"
 )
 
 // Advent of Code 2022
 // Day 15, part a
 
+type sensor struct {
+	p image.Point
+	b image.Point
+}
+
 func main() {
-	sum := 0
+	var sensors []sensor
 	for _, line := range exp.MustReadLines("inputs/15.txt") {
-		sum += exp.Must(strconv.Atoi(line))
+		var s sensor
+		exp.Must(fmt.Sscanf(line, "Sensor at x=%d, y=%d: closest beacon is at x=%d, y=%d", &s.p.X, &s.p.Y, &s.b.X, &s.b.Y))
+		sensors = append(sensors, s)
 	}
-	fmt.Println(sum)
+
+	const row = 2000000
+	poss := make(algo.Set[int])
+
+	for _, s := range sensors {
+		r := algo.L1(s.p.Sub(s.b))
+		d := algo.Abs(row - s.p.Y)
+		// Area intersects line at all?
+		rd := r - d
+		if rd < 0 {
+			continue
+		}
+
+		for x := s.p.X - rd; x <= s.p.X+rd; x++ {
+			poss.Insert(x)
+		}
+	}
+
+	for _, s := range sensors {
+		if s.b.Y == row {
+			delete(poss, s.b.X)
+		}
+	}
+
+	fmt.Println(len(poss))
 }
--- 2022/15b.go
+++ 2022/15b.go
@@ -2,18 +2,51 @@ package main
 
 import (
 	"fmt"
-	"strconv"
+	"image"
 
 	"github.com/DrJosh9000/exp"
+	"github.com/DrJosh9000/exp/algo"
 )
 
 // Advent of Code 2022
 // Day 15, part b
 
+type sensor struct {
+	p image.Point
+	b image.Point
+}
+
 func main() {
-	sum := 0
+	var sensors []sensor
 	for _, line := range exp.MustReadLines("inputs/15.txt") {
-		sum += exp.Must(strconv.Atoi(line))
+		var s sensor
+		exp.Must(fmt.Sscanf(line, "Sensor at x=%d, y=%d: closest beacon is at x=%d, y=%d", &s.p.X, &s.p.Y, &s.b.X, &s.b.Y))
+		sensors = append(sensors, s)
 	}
-	fmt.Println(sum)
+
+	bounds := image.Rect(0, 0, 4000001, 4000001)
+
+	for _, s := range sensors {
+		r := algo.L1(s.p.Sub(s.b)) + 1
+
+		// Walk around the circumference
+		var d image.Point
+		for d.X = -r; d.X <= r; d.X++ {
+			adx := algo.Abs(d.X)
+		coordLoop:
+			for _, d.Y = range []int{r - adx, adx - r} {
+				p := s.p.Add(d)
+				if !p.In(bounds) {
+					continue
+				}
+				for _, t := range sensors {
+					if algo.L1(p.Sub(t.p)) < algo.L1(t.p.Sub(t.b)) {
+						continue coordLoop
+					}
+				}
+				fmt.Println(p.X*4000000 + p.Y)
+				return
+			}
+		}
+	}
 }
