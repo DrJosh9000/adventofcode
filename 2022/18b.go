3db47664b2655eb209a5a896659705de61d0c9c3
Josh Deprez <josh.deprez@gmail.com>
2022-12-18 16:33:21 +1100

    2022/18b: optimised a little bit

--- 2022/18b.go
+++ 2022/18b.go
@@ -21,32 +21,43 @@ var neigh = []algo.Vec3[int]{
 }
 
 func main() {
-	var min algo.Vec3[int]
+	var min, max algo.Vec3[int]
 	lava := make(algo.Set[algo.Vec3[int]])
 	for i, line := range exp.MustReadLines("inputs/18.txt") {
 		var v algo.Vec3[int]
 		exp.Must(fmt.Sscanf(line, "%d,%d,%d", &v[0], &v[1], &v[2]))
 		lava.Insert(v)
 		if i == 0 {
-			min = v
+			min, max = v, v
 			continue
 		}
 		min[0] = algo.Min(min[0], v[0])
 		min[1] = algo.Min(min[1], v[1])
 		min[2] = algo.Min(min[2], v[2])
+		max[0] = algo.Max(max[0], v[0])
+		max[1] = algo.Max(max[1], v[1])
+		max[2] = algo.Max(max[2], v[2])
 	}
 
-	ext := errors.New("exterior face")
-	outside := algo.Set[algo.Vec3[int]]{min.Add(neigh[1]): {}}
+	intErr := errors.New("interior face")
+	extErr := errors.New("exterior face")
+	inside := make(algo.Set[algo.Vec3[int]])
+	outside := make(algo.Set[algo.Vec3[int]])
+	outside.Insert(min.Add(neigh[1]))
+	outside.Insert(max.Add(neigh[0]))
 	sa := 0
 	for c := range lava {
 		for _, n := range neigh {
-			if lava.Contains(c.Add(n)) {
+			p := c.Add(n)
+			if lava.Contains(p) {
 				continue
 			}
-			if _, err := algo.FloodFill(c.Add(n), func(x algo.Vec3[int], _ int) ([]algo.Vec3[int], error) {
+			if _, err := algo.FloodFill(p, func(x algo.Vec3[int], _ int) ([]algo.Vec3[int], error) {
+				if inside.Contains(x) {
+					return nil, intErr
+				}
 				if outside.Contains(x) {
-					return nil, ext
+					return nil, extErr
 				}
 				var next []algo.Vec3[int]
 				for _, n := range neigh {
@@ -56,10 +67,11 @@ func main() {
 					next = append(next, x.Add(n))
 				}
 				return next, nil
-			}); err != ext {
+			}); err != extErr {
+				inside.Insert(p)
 				continue
 			}
-			outside.Insert(c.Add(n))
+			outside.Insert(p)
 			sa++
 		}
 	}
