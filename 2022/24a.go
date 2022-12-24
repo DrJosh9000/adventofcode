4cbdf258c441399b3a2238a18b7d6fa1add01795
Josh Deprez <josh.deprez@gmail.com>
2022-12-24 16:53:04 +1100

    2022/24

--- 2022/24a.go
+++ 2022/24a.go
@@ -1,19 +1,104 @@
 package main
 
 import (
+	"errors"
 	"fmt"
-	"strconv"
+	"image"
 
 	"github.com/DrJosh9000/exp"
+	"github.com/DrJosh9000/exp/algo"
+	"github.com/DrJosh9000/exp/grid"
 )
 
 // Advent of Code 2022
 // Day 24, part a
 
 func main() {
-	sum := 0
-	for _, line := range exp.MustReadLines("inputs/24.txt") {
-		sum += exp.Must(strconv.Atoi(line))
+	input := exp.MustReadByteGrid("inputs/24.txt")
+
+	var start, end image.Point
+	for x, c := range input[0] {
+		if c == '.' {
+			start.X = x
+			break
+		}
 	}
-	fmt.Println(sum)
+	end.Y = len(input) - 1
+	for x, c := range input[len(input)-1] {
+		if c == '.' {
+			end.X = x
+		}
+	}
+
+	storm := make([]grid.Sparse[[]image.Point], 1)
+	storm[0] = grid.MapToSparse(input, func(b byte) ([]image.Point, bool) {
+		if b == '.' || b == '#' {
+			return nil, false
+		}
+		return []image.Point{algo.CGVL[rune(b)]}, true
+	})
+
+	valley := input.Bounds().Inset(1)
+	dx, dy := valley.Dx(), valley.Dy()
+	evolve := func() {
+		st2 := make(grid.Sparse[[]image.Point], len(storm[0]))
+		for p, ds := range storm[len(storm)-1] {
+			for _, d := range ds {
+				q := p.Add(d)
+				if !q.In(valley) {
+					q.X = (q.X-1+dx)%dx + 1
+					q.Y = (q.Y-1+dy)%dy + 1
+				}
+				st2[q] = append(st2[q], d)
+			}
+		}
+
+		// st2d, _ := st2.ToDense()
+		// fmt.Println(grid.Map(st2d, func(ps []image.Point) byte {
+		// 	if len(ps) == 0 {
+		// 		return '.'
+		// 	}
+		// 	return '0' + byte(len(ps))
+		// }))
+
+		storm = append(storm, st2)
+	}
+
+	type state struct {
+		p image.Point
+		t int
+	}
+	steps := append(algo.Neigh4, image.Point{})
+
+	algo.FloodFill(state{p: start}, func(s state, t int) ([]state, error) {
+		//fmt.Println(p, t)
+		if s.p == end {
+			fmt.Println(t)
+			return nil, errors.New("all done")
+		}
+		t++
+		for t >= len(storm) {
+			evolve()
+		}
+		var next []state
+		for _, d := range steps {
+			q := s.p.Add(d)
+			if !q.In(valley) && q != start && q != end {
+				continue
+			}
+			if len(storm[t][q]) > 0 {
+				continue
+			}
+			next = append(next, state{p: q, t: t})
+		}
+		//fmt.Println(next)
+		return next, nil
+	})
+
+	// p := end
+	// for p != start {
+	// 	fmt.Println(p)
+	// 	p = prev[p]
+	// }
+	// fmt.Println(start)
 }
--- 2022/24b.go
+++ 2022/24b.go
@@ -1,19 +1,97 @@
 package main
 
 import (
+	"errors"
 	"fmt"
-	"strconv"
+	"image"
 
 	"github.com/DrJosh9000/exp"
+	"github.com/DrJosh9000/exp/algo"
+	"github.com/DrJosh9000/exp/grid"
 )
 
 // Advent of Code 2022
 // Day 24, part b
 
 func main() {
-	sum := 0
-	for _, line := range exp.MustReadLines("inputs/24.txt") {
-		sum += exp.Must(strconv.Atoi(line))
+	input := exp.MustReadByteGrid("inputs/24.txt")
+
+	var start, end image.Point
+	for x, c := range input[0] {
+		if c == '.' {
+			start.X = x
+			break
+		}
 	}
-	fmt.Println(sum)
+	end.Y = len(input) - 1
+	for x, c := range input[len(input)-1] {
+		if c == '.' {
+			end.X = x
+		}
+	}
+
+	storm := make([]grid.Sparse[[]image.Point], 1)
+	storm[0] = grid.MapToSparse(input, func(b byte) ([]image.Point, bool) {
+		if b == '.' || b == '#' {
+			return nil, false
+		}
+		return []image.Point{algo.CGVL[rune(b)]}, true
+	})
+
+	valley := input.Bounds().Inset(1)
+	dx, dy := valley.Dx(), valley.Dy()
+	evolve := func() {
+		st2 := make(grid.Sparse[[]image.Point], len(storm[0]))
+		for p, ds := range storm[len(storm)-1] {
+			for _, d := range ds {
+				q := p.Add(d)
+				if !q.In(valley) {
+					q.X = (q.X-1+dx)%dx + 1
+					q.Y = (q.Y-1+dy)%dy + 1
+				}
+				st2[q] = append(st2[q], d)
+			}
+		}
+
+		storm = append(storm, st2)
+	}
+
+	type state struct {
+		p        image.Point
+		t        int
+		wp1, wp2 bool
+	}
+	steps := append(algo.Neigh4, image.Point{})
+
+	algo.FloodFill(state{p: start}, func(s state, t int) ([]state, error) {
+		switch {
+		case s.p == end && s.wp1 && s.wp2:
+			fmt.Println(t)
+			return nil, errors.New("all done")
+		case s.p == start && s.wp1:
+			s.wp2 = true
+		case s.p == end:
+			s.wp1 = true
+		}
+
+		t++
+		for t >= len(storm) {
+			evolve()
+		}
+		var next []state
+		for _, d := range steps {
+			q := s.p.Add(d)
+			if !q.In(valley) && q != start && q != end {
+				continue
+			}
+			if len(storm[t][q]) > 0 {
+				continue
+			}
+			s2 := s
+			s2.p = q
+			s2.t = t
+			next = append(next, s2)
+		}
+		return next, nil
+	})
 }
