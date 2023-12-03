12688a333e305787891ad7377e5d4bd7f68d64bd
Josh Deprez <josh.deprez@gmail.com>
2023-12-03 16:32:50 +1100

    2023/3

--- 2023/3a/main.go
+++ 2023/3a/main.go
@@ -2,18 +2,55 @@ package main
 
 import (
 	"fmt"
+	"image"
+	"regexp"
 	"strconv"
 
 	"github.com/DrJosh9000/exp"
+	"github.com/DrJosh9000/exp/grid"
 )
 
 // Advent of Code 2023
 // Day 3, part a
 
 func main() {
+	digitsRE := regexp.MustCompile(`\d+`)
+
 	sum := 0
-	for _, line := range exp.MustReadLines("2023/inputs/3.txt") {
-		sum += exp.Must(strconv.Atoi(line))
+	g := grid.BytesFromStrings(exp.MustReadLines("2023/inputs/3.txt"))
+	bounds := g.Bounds()
+	for y, row := range g {
+		for _, rg := range digitsRE.FindAllIndex(row, -1) {
+			sym := byte('.')
+
+		pointLoop:
+			for p := image.Pt(rg[0], y); p.X < rg[1]; p.X++ {
+				ds := []image.Point{{0, -1}, {0, 1}}
+				if p.X == rg[0] {
+					ds = append(ds, image.Pt(-1, -1), image.Pt(-1, 0), image.Pt(-1, 1))
+				}
+				if p.X == rg[1]-1 {
+					ds = append(ds, image.Pt(1, -1), image.Pt(1, 0), image.Pt(1, 1))
+				}
+				for _, d := range ds {
+					q := p.Add(d)
+					if !q.In(bounds) {
+						continue
+					}
+					sym = g[q.Y][q.X]
+					if sym != '.' {
+						break pointLoop
+					}
+				}
+			}
+
+			if sym == '.' {
+				continue
+			}
+			sum += exp.Must(strconv.Atoi(string(row[rg[0]:rg[1]])))
+		}
+
 	}
+
 	fmt.Println(sum)
 }
--- 2023/3b/main.go
+++ 2023/3b/main.go
@@ -2,18 +2,49 @@ package main
 
 import (
 	"fmt"
+	"image"
+	"regexp"
 	"strconv"
 
 	"github.com/DrJosh9000/exp"
+	"github.com/DrJosh9000/exp/grid"
 )
 
 // Advent of Code 2023
 // Day 3, part b
 
 func main() {
-	sum := 0
-	for _, line := range exp.MustReadLines("2023/inputs/3.txt") {
-		sum += exp.Must(strconv.Atoi(line))
+	digitsRE := regexp.MustCompile(`\d+`)
+
+	g := grid.BytesFromStrings(exp.MustReadLines("2023/inputs/3.txt"))
+
+	stars := make(map[image.Point][]int)
+	for y, row := range g {
+		for x, c := range row {
+			if c == '*' {
+				stars[image.Pt(x, y)] = []int{}
+			}
+		}
 	}
+
+	for y, row := range g {
+		for _, rg := range digitsRE.FindAllIndex(row, -1) {
+			n := exp.Must(strconv.Atoi(string(row[rg[0]:rg[1]])))
+			for s := range stars {
+				if s.Y >= y-1 && s.Y <= y+1 && s.X >= rg[0]-1 && s.X <= rg[1] {
+					stars[s] = append(stars[s], n)
+				}
+			}
+		}
+	}
+
+	sum := 0
+	for _, ns := range stars {
+		if len(ns) != 2 {
+			continue
+		}
+		sum += ns[0] * ns[1]
+	}
+
 	fmt.Println(sum)
 }
