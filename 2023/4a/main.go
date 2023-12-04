79e8600d190190ac43b0e98f3e25f06f088dd219
Josh Deprez <josh.deprez@gmail.com>
2023-12-04 16:15:57 +1100

    2023/4

--- 2023/4a/main.go
+++ 2023/4a/main.go
@@ -2,9 +2,12 @@ package main
 
 import (
 	"fmt"
+	"log"
 	"strconv"
+	"strings"
 
 	"github.com/DrJosh9000/exp"
+	"github.com/DrJosh9000/exp/algo"
 )
 
 // Advent of Code 2023
@@ -13,7 +16,31 @@ import (
 func main() {
 	sum := 0
 	for _, line := range exp.MustReadLines("2023/inputs/4.txt") {
-		sum += exp.Must(strconv.Atoi(line))
+		_, rest, ok := strings.Cut(line, ": ")
+		if !ok {
+			log.Fatalf("no colon in %q", line)
+		}
+		wins, haves, ok := strings.Cut(rest, " | ")
+		if !ok {
+			log.Fatalf("no pipe in %q", line)
+		}
+		winm := make(algo.Set[int])
+		for _, ws := range strings.Fields(wins) {
+			winm.Insert(exp.Must(strconv.Atoi(ws)))
+		}
+		x := 0
+		for _, hs := range strings.Fields(haves) {
+			h := exp.Must(strconv.Atoi(hs))
+			if !winm.Contains(h) {
+				continue
+			}
+			if x == 0 {
+				x = 1
+			} else {
+				x *= 2
+			}
+		}
+		sum += x
 	}
 	fmt.Println(sum)
 }
--- 2023/4b/main.go
+++ 2023/4b/main.go
@@ -2,18 +2,52 @@ package main
 
 import (
 	"fmt"
+	"log"
 	"strconv"
+	"strings"
 
 	"github.com/DrJosh9000/exp"
+	"github.com/DrJosh9000/exp/algo"
 )
 
 // Advent of Code 2023
 // Day 4, part b
 
 func main() {
+	lines := exp.MustReadLines("2023/inputs/4.txt")
+	table := make([]int, len(lines)+1)
+	for i := range table {
+		table[i] = 1
+	}
+
 	sum := 0
-	for _, line := range exp.MustReadLines("2023/inputs/4.txt") {
-		sum += exp.Must(strconv.Atoi(line))
+	for _, line := range lines {
+		cardn, rest, ok := strings.Cut(line, ": ")
+		if !ok {
+			log.Fatalf("no colon in %q", line)
+		}
+		wins, haves, ok := strings.Cut(rest, " | ")
+		if !ok {
+			log.Fatalf("no pipe in %q", line)
+		}
+		cardnum := exp.Must(strconv.Atoi(strings.Fields(cardn)[1]))
+		winm := make(algo.Set[int])
+		for _, ws := range strings.Fields(wins) {
+			winm.Insert(exp.Must(strconv.Atoi(ws)))
+		}
+		matches := 0
+		for _, hs := range strings.Fields(haves) {
+			h := exp.Must(strconv.Atoi(hs))
+			if !winm.Contains(h) {
+				continue
+			}
+			matches++
+		}
+		sum += table[cardnum]
+		for matches > 0 {
+			table[cardnum+matches] += table[cardnum]
+			matches--
+		}
 	}
 	fmt.Println(sum)
 }
