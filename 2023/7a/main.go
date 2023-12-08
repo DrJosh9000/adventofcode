9b808b3660ef5b79f745358b832bcdf1fc96c285
Josh Deprez <josh.deprez@gmail.com>
2023-12-08 14:55:45 +1100

    2023/7

--- 2023/7a/main.go
+++ 2023/7a/main.go
@@ -2,18 +2,93 @@ package main
 
 import (
 	"fmt"
-	"strconv"
+	"strings"
 
 	"github.com/DrJosh9000/exp"
+	"github.com/DrJosh9000/exp/algo"
 )
 
 // Advent of Code 2023
 // Day 7, part a
 
-func main() {
-	sum := 0
-	for _, line := range exp.MustReadLines("2023/inputs/7.txt") {
-		sum += exp.Must(strconv.Atoi(line))
+const cards = "23456789TJQKA"
+
+const (
+	highCard = iota
+	onePair
+	twoPair
+	threeOfKind
+	fullHouse
+	fourOfKind
+	fiveOfKind
+)
+
+func handType(hand string) int {
+	hist := algo.Freq([]rune(hand))
+	switch len(hist) {
+	case 1:
+		return fiveOfKind
+
+	case 2:
+		for _, n := range hist {
+			if n == 1 || n == 4 {
+				return fourOfKind
+			}
+			return fullHouse
+		}
+
+	case 3:
+		for _, n := range hist {
+			if n == 3 {
+				return threeOfKind
+			}
+			if n == 2 {
+				return twoPair
+			}
+		}
+
+	case 4:
+		return onePair
+
+	case 5:
+		return highCard
 	}
+	panic("couldn't classify hand " + hand)
+}
+
+func main() {
+	type handBid struct {
+		hand string
+		bid  int
+	}
+
+	var input []handBid
+
+	for _, line := range exp.MustReadLines("2023/inputs/7.txt") {
+		var hand handBid
+		exp.Must(fmt.Sscanf(line, "%s %d", &hand.hand, &hand.bid))
+		input = append(input, hand)
+	}
+
+	algo.SortSlice(input, func(a, b handBid) bool {
+		at, bt := handType(a.hand), handType(b.hand)
+		if at != bt {
+			return at < bt
+		}
+		for k := 0; k < 5; k++ {
+			ac, bc := a.hand[k], b.hand[k]
+			if ac == bc {
+				continue
+			}
+			return strings.IndexByte(cards, ac) < strings.IndexByte(cards, bc)
+		}
+		return false
+	})
+
+	sum := 0
+	for i, h := range input {
+		sum += (i + 1) * h.bid
+	}
+
 	fmt.Println(sum)
 }
