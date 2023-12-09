06c27d7be7391fb8b3a02d1f7ebef879cabc39d5
Josh Deprez <josh.deprez@gmail.com>
2023-12-09 16:52:11 +1100

    2023/9

--- 2023/9a/main.go
+++ 2023/9a/main.go
@@ -2,8 +2,12 @@ package main
 
 import (
 	"fmt"
+	"strconv"
+	"strings"
 
 	"github.com/DrJosh9000/exp"
+	"github.com/DrJosh9000/exp/algo"
+	"github.com/DrJosh9000/exp/para"
 )
 
 // Advent of Code 2023
@@ -11,11 +15,38 @@ import (
 
 const inputPath = "2023/inputs/9.txt"
 
+func diffs(in []int) ([]int, bool) {
+	allZ := true
+	out := make([]int, len(in)-1)
+	for i := range out {
+		out[i] = in[i+1] - in[i]
+		allZ = allZ && out[i] == 0
+	}
+	return out, allZ
+}
+
+func last[S ~[]E, E any](s S) E {
+	return s[len(s)-1]
+}
+
 func main() {
 	lines := exp.MustReadLines(inputPath)
-	sum := 0
-	for _, line := range lines {
-		sum += exp.MustAtoi(line)
-	}
-	fmt.Println(sum)
+	fmt.Println(algo.Sum(para.Map(lines, func(line string) int {
+		nums := [][]int{
+			algo.MustMap(strings.Fields(line), strconv.Atoi),
+		}
+		for {
+			ds, allZ := diffs(last(nums))
+			nums = append(nums, ds)
+			if allZ {
+				break
+			}
+		}
+		//fmt.Println(nums)
+		for i := range nums[1:] {
+			j := len(nums) - i - 1
+			nums[j-1] = append(nums[j-1], last(nums[j-1])+last(nums[j]))
+		}
+		return last(nums[0])
+	})))
 }
