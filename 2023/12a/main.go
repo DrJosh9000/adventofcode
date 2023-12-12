3a48a77a185e0cca0eaa3d322ae9f4e0e43ecfe9
Josh Deprez <josh.deprez@gmail.com>
2023-12-12 17:31:20 +1100

    2023/12

--- 2023/12a/main.go
+++ 2023/12a/main.go
@@ -2,8 +2,12 @@ package main
 
 import (
 	"fmt"
+	"log"
+	"strconv"
+	"strings"
 
 	"github.com/DrJosh9000/exp"
+	"github.com/DrJosh9000/exp/algo"
 )
 
 // Advent of Code 2023
@@ -13,9 +17,49 @@ const inputPath = "2023/inputs/12.txt"
 
 func main() {
 	lines := exp.MustReadLines(inputPath)
-	sum := 0
-	for _, line := range lines {
-		sum += exp.MustAtoi(line)
-	}
-	fmt.Println(sum)
+
+	fmt.Println(algo.Sum(algo.Map(lines, func(line string) int {
+		springs, broks, ok := strings.Cut(line, " ")
+		if !ok {
+			log.Fatalf("couldn't cut line %q", line)
+		}
+		brok := algo.MustMap(strings.Split(broks, ","), strconv.Atoi)
+
+		springs = "." + springs + "."
+		pattern := []byte{'.'}
+		for _, b := range brok {
+			for n := 0; n < b; n++ {
+				pattern = append(pattern, '#')
+			}
+			pattern = append(pattern, '.')
+		}
+		n1 := len(pattern) - 1
+
+		count := make([]int, len(pattern))
+		count[0] = 1
+
+		for _, c := range []byte(springs) {
+			nc := make([]int, len(pattern))
+
+			for j, p := range pattern {
+				switch c {
+				case p, '?':
+					if j == 0 {
+						nc[j] = count[j]
+						break
+					}
+					switch p {
+					case '#':
+						nc[j] = count[j-1]
+					case '.':
+						nc[j] = count[j] + count[j-1]
+					}
+				}
+			}
+
+			count = nc
+		}
+		//fmt.Printf("%s: %d\n", line, count)
+		return count[n1]
+	})))
 }
