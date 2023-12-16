77481357e4ac69b202e84758f60e7e509b16c919
Josh Deprez <josh.deprez@gmail.com>
2023-12-16 16:20:17 +1100

    2023/16

--- 2023/16a/main.go
+++ 2023/16a/main.go
@@ -2,8 +2,12 @@ package main
 
 import (
 	"fmt"
+	"image"
+	"sync"
 
 	"github.com/DrJosh9000/exp"
+	"github.com/DrJosh9000/exp/algo"
+	"github.com/DrJosh9000/exp/para"
 )
 
 // Advent of Code 2023
@@ -11,11 +15,89 @@ import (
 
 const inputPath = "2023/inputs/16.txt"
 
+var up, down, left, right = image.Pt(0, -1), image.Pt(0, 1), image.Pt(-1, 0), image.Pt(1, 0)
+
 func main() {
-	lines := exp.MustReadLines(inputPath)
-	sum := 0
-	for _, line := range lines {
-		sum += exp.MustAtoi(line)
+	g := exp.MustReadByteGrid(inputPath)
+	bounds := g.Bounds()
+
+	type beam struct {
+		p, d image.Point
 	}
-	fmt.Println(sum)
+	var µ sync.Mutex
+	energised := make(algo.Set[image.Point])
+	seen := make(algo.Set[beam])
+	q := para.NewQueue(beam{d: right})
+	q.Process(func(b beam) {
+		if !b.p.In(bounds) {
+			return
+		}
+
+		µ.Lock()
+		if seen.Contains(b) {
+			µ.Unlock()
+			return
+		}
+		seen.Insert(b)
+		energised.Insert(b.p)
+		µ.Unlock()
+
+		switch g[b.p.Y][b.p.X] {
+		case '.':
+			b.p = b.p.Add(b.d)
+			q.Push(b)
+
+		case '\\':
+			switch b.d {
+			case up:
+				b.d = left
+			case down:
+				b.d = right
+			case right:
+				b.d = down
+			case left:
+				b.d = up
+			}
+			b.p = b.p.Add(b.d)
+			q.Push(b)
+
+		case '/':
+			switch b.d {
+			case up:
+				b.d = right
+			case down:
+				b.d = left
+			case right:
+				b.d = up
+			case left:
+				b.d = down
+			}
+			b.p = b.p.Add(b.d)
+			q.Push(b)
+
+		case '|':
+			switch b.d {
+			case left, right:
+				q.Push(beam{p: b.p.Add(up), d: up})
+				q.Push(beam{p: b.p.Add(down), d: down})
+
+			case up, down:
+				b.p = b.p.Add(b.d)
+				q.Push(b)
+			}
+
+		case '-':
+			switch b.d {
+			case up, down:
+				q.Push(beam{p: b.p.Add(left), d: left})
+				q.Push(beam{p: b.p.Add(right), d: right})
+
+			case left, right:
+				b.p = b.p.Add(b.d)
+				q.Push(b)
+			}
+		}
+	})
+
+	fmt.Println(len(energised))
 }
--- 2023/16b/main.go
+++ 2023/16b/main.go
@@ -2,8 +2,13 @@ package main
 
 import (
 	"fmt"
+	"image"
+	"sync"
 
 	"github.com/DrJosh9000/exp"
+	"github.com/DrJosh9000/exp/algo"
+	"github.com/DrJosh9000/exp/grid"
+	"github.com/DrJosh9000/exp/para"
 )
 
 // Advent of Code 2023
@@ -11,11 +16,108 @@ import (
 
 const inputPath = "2023/inputs/16.txt"
 
+var up, down, left, right = image.Pt(0, -1), image.Pt(0, 1), image.Pt(-1, 0), image.Pt(1, 0)
+
+type beam struct {
+	p, d image.Point
+}
+
 func main() {
-	lines := exp.MustReadLines(inputPath)
-	sum := 0
-	for _, line := range lines {
-		sum += exp.MustAtoi(line)
+	g := exp.MustReadByteGrid(inputPath)
+	h, w := g.Size()
+
+	best := 0
+	for x := range g[0] {
+		best = max(best,
+			countEnergised(g, beam{p: image.Pt(x, 0), d: down}),
+			countEnergised(g, beam{p: image.Pt(x, h-1), d: up}),
+		)
 	}
-	fmt.Println(sum)
+	for y := range g {
+		best = max(best,
+			countEnergised(g, beam{p: image.Pt(0, y), d: right}),
+			countEnergised(g, beam{p: image.Pt(w-1, y), d: left}),
+		)
+	}
+	fmt.Println(best)
+}
+
+func countEnergised(g grid.Dense[byte], b beam) int {
+	bounds := g.Bounds()
+
+	var µ sync.Mutex
+	energised := make(algo.Set[image.Point])
+	seen := make(algo.Set[beam])
+	q := para.NewQueue(b)
+	q.Process(func(b beam) {
+		if !b.p.In(bounds) {
+			return
+		}
+
+		µ.Lock()
+		if seen.Contains(b) {
+			µ.Unlock()
+			return
+		}
+		seen.Insert(b)
+		energised.Insert(b.p)
+		µ.Unlock()
+
+		switch g[b.p.Y][b.p.X] {
+		case '.':
+			b.p = b.p.Add(b.d)
+			q.Push(b)
+
+		case '\\':
+			switch b.d {
+			case up:
+				b.d = left
+			case down:
+				b.d = right
+			case right:
+				b.d = down
+			case left:
+				b.d = up
+			}
+			b.p = b.p.Add(b.d)
+			q.Push(b)
+
+		case '/':
+			switch b.d {
+			case up:
+				b.d = right
+			case down:
+				b.d = left
+			case right:
+				b.d = up
+			case left:
+				b.d = down
+			}
+			b.p = b.p.Add(b.d)
+			q.Push(b)
+
+		case '|':
+			switch b.d {
+			case left, right:
+				q.Push(beam{p: b.p.Add(up), d: up})
+				q.Push(beam{p: b.p.Add(down), d: down})
+
+			case up, down:
+				b.p = b.p.Add(b.d)
+				q.Push(b)
+			}
+
+		case '-':
+			switch b.d {
+			case up, down:
+				q.Push(beam{p: b.p.Add(left), d: left})
+				q.Push(beam{p: b.p.Add(right), d: right})
+
+			case left, right:
+				b.p = b.p.Add(b.d)
+				q.Push(b)
+			}
+		}
+	})
+	return len(energised)
 }
