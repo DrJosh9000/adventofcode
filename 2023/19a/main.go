6d1504926a3af9b53a1d00e1ad37ffa22f4936f8
Josh Deprez <josh.deprez@gmail.com>
2023-12-19 17:10:22 +1100

    2023/19

--- 2023/19a/main.go
+++ 2023/19a/main.go
@@ -2,8 +2,11 @@ package main
 
 import (
 	"fmt"
+	"strings"
+	"sync/atomic"
 
 	"github.com/DrJosh9000/exp"
+	"github.com/DrJosh9000/exp/para"
 )
 
 // Advent of Code 2023
@@ -11,11 +14,110 @@ import (
 
 const inputPath = "2023/inputs/19.txt"
 
+type rule struct {
+	varn, cmp, dest string
+	thres           int
+}
+
+type part struct {
+	wf         string
+	x, m, a, s int
+}
+
+func (r *rule) test(p part) bool {
+	var v int
+	switch r.varn {
+	case "":
+		return true
+	case "x":
+		v = p.x
+	case "m":
+		v = p.m
+	case "a":
+		v = p.a
+	case "s":
+		v = p.s
+	default:
+		panic("unknown variable " + r.varn)
+	}
+	switch r.cmp {
+	case "<":
+		return v < r.thres
+	case ">":
+		return v > r.thres
+	default:
+		panic("unknown comparison " + r.cmp)
+	}
+}
+
 func main() {
 	lines := exp.MustReadLines(inputPath)
-	sum := 0
+
+	var parts []part
+	byName := make(map[string][]rule)
+	parsingWFs := true
 	for _, line := range lines {
-		sum += exp.MustAtoi(line)
+		if line == "" && parsingWFs {
+			parsingWFs = false
+			continue
+		}
+		if parsingWFs {
+			var wf []rule
+			name, body, ok := strings.Cut(line, "{")
+			if !ok {
+				panic("invalid workflow " + line)
+			}
+			for _, rs := range strings.Split(strings.TrimSuffix(body, "}"), ",") {
+				cond, dest, ok := strings.Cut(rs, ":")
+				var r rule
+				if ok {
+					r = rule{
+						varn:  cond[:1],
+						cmp:   cond[1:2],
+						dest:  dest,
+						thres: exp.MustAtoi(cond[2:]),
+					}
+				} else {
+					r = rule{dest: cond}
+				}
+				wf = append(wf, r)
+			}
+			byName[name] = wf
+			continue
+		}
+		// parsing parts
+		var p part
+		exp.MustSscanf(line, "{x=%d,m=%d,a=%d,s=%d}", &p.x, &p.m, &p.a, &p.s)
+		p.wf = "in"
+		parts = append(parts, p)
 	}
-	fmt.Println(sum)
+
+	var sum atomic.Int64
+	q := para.NewQueue(parts...)
+	q.Process(func(p part) {
+		wf, ok := byName[p.wf]
+		if !ok {
+			panic("unknown workflow " + p.wf)
+		}
+		for _, r := range wf {
+			if !r.test(p) {
+				continue
+			}
+			switch r.dest {
+			case "A":
+				sum.Add(int64(p.x + p.m + p.a + p.s))
+				return
+			case "R":
+				// drop
+				return
+			default:
+				p.wf = r.dest
+				q.Push(p)
+				return
+			}
+		}
+		panic(fmt.Sprintf("part %v didn't match any rule in workflow %v", p, wf))
+	})
+
+	fmt.Println(sum.Load())
 }
--- 2023/19b/main.go
+++ 2023/19b/main.go
@@ -2,8 +2,12 @@ package main
 
 import (
 	"fmt"
+	"strings"
+	"sync/atomic"
 
 	"github.com/DrJosh9000/exp"
+	"github.com/DrJosh9000/exp/algo"
+	"github.com/DrJosh9000/exp/para"
 )
 
 // Advent of Code 2023
@@ -11,11 +15,133 @@ import (
 
 const inputPath = "2023/inputs/19.txt"
 
+type rule struct {
+	varn, cmp, dest string
+	thres           int
+}
+
+type part struct {
+	wf         string
+	x, m, a, s []algo.Range[int]
+}
+
+func rlen(r algo.Range[int]) int { return r.Max - r.Min + 1 }
+
+func (r *rule) act(p part) (acc, pass part) {
+	acc, pass = p, p
+
+	var v, vacc, vpass *[]algo.Range[int]
+	switch r.varn {
+	case "":
+		return p, part{}
+	case "x":
+		v, vacc, vpass = &p.x, &acc.x, &pass.x
+	case "m":
+		v, vacc, vpass = &p.m, &acc.m, &pass.m
+	case "a":
+		v, vacc, vpass = &p.a, &acc.a, &pass.a
+	case "s":
+		v, vacc, vpass = &p.s, &acc.s, &pass.s
+	default:
+		panic("unknown variable " + r.varn)
+	}
+
+	var sub algo.Range[int]
+	switch r.cmp {
+	case "<":
+		sub = algo.NewRange(r.thres, 4000)
+
+	case ">":
+		sub = algo.NewRange(1, r.thres)
+
+	default:
+		panic("unknown comparison " + r.cmp)
+	}
+
+	var va, vp []algo.Range[int]
+	for _, vr := range *v {
+		va = append(va, algo.RangeSubtract(vr, sub)...)
+		in := vr.Intersection(sub)
+		if !in.IsEmpty() {
+			vp = append(vp, in)
+		}
+	}
+	*vacc = va
+	*vpass = vp
+	return acc, pass
+}
+
 func main() {
 	lines := exp.MustReadLines(inputPath)
-	sum := 0
+
+	byName := make(map[string][]rule)
 	for _, line := range lines {
-		sum += exp.MustAtoi(line)
+		if line == "" {
+			break
+		}
+		var wf []rule
+		name, body, ok := strings.Cut(line, "{")
+		if !ok {
+			panic("invalid workflow " + line)
+		}
+		for _, rs := range strings.Split(strings.TrimSuffix(body, "}"), ",") {
+			cond, dest, ok := strings.Cut(rs, ":")
+			var r rule
+			if ok {
+				r = rule{
+					varn:  cond[:1],
+					cmp:   cond[1:2],
+					dest:  dest,
+					thres: exp.MustAtoi(cond[2:]),
+				}
+			} else {
+				r = rule{dest: cond}
+			}
+			wf = append(wf, r)
+		}
+		byName[name] = wf
 	}
-	fmt.Println(sum)
+
+	full := []algo.Range[int]{algo.NewRange(1, 4000)}
+	var sum atomic.Int64
+	q := para.NewQueue(part{
+		wf: "in",
+		x:  full,
+		m:  full,
+		a:  full,
+		s:  full,
+	})
+	q.Process(func(p part) {
+		wf, ok := byName[p.wf]
+		if !ok {
+			panic("unknown workflow " + p.wf)
+		}
+		for _, r := range wf {
+			if len(p.x) == 0 || len(p.m) == 0 || len(p.a) == 0 || len(p.s) == 0 {
+				return
+			}
+
+			acc, pass := r.act(p)
+
+			switch r.dest {
+			case "A":
+				x := algo.Sum(algo.Map(acc.x, rlen))
+				m := algo.Sum(algo.Map(acc.m, rlen))
+				a := algo.Sum(algo.Map(acc.a, rlen))
+				s := algo.Sum(algo.Map(acc.s, rlen))
+				sum.Add(int64(x * m * a * s))
+
+			case "R":
+				// nop
+
+			default:
+				acc.wf = r.dest
+				q.Push(acc)
+			}
+
+			p = pass
+		}
+	})
+
+	fmt.Println(sum.Load())
 }
