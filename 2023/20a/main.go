497f1c460925b8f921f83a59948bb40b8ea1ea73
Josh Deprez <josh.deprez@gmail.com>
2023-12-20 21:05:07 +1100

    2023/20

--- 2023/20a/main.go
+++ 2023/20a/main.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"fmt"
+	"strings"
 
 	"github.com/DrJosh9000/exp"
 )
@@ -11,11 +12,149 @@ import (
 
 const inputPath = "2023/inputs/20.txt"
 
+type signal bool
+
+const (
+	high = signal(false)
+	low  = signal(true)
+)
+
+func (s signal) String() string {
+	if s == high {
+		return "high"
+	}
+	return "low"
+}
+
+type module interface {
+	outs() []string
+	process(signal, string) (signal, bool)
+}
+
+type broadcaster struct {
+	out []string
+}
+
+func (m *broadcaster) process(s signal, _ string) (signal, bool) { return s, true }
+
+type flipflop struct {
+	out   []string
+	state bool
+}
+
+func (m *flipflop) process(s signal, _ string) (signal, bool) {
+	if s == high {
+		return s, false
+	}
+	m.state = !m.state
+	if m.state {
+		return high, true
+	}
+	return low, true
+}
+
+type conjunction struct {
+	out  []string
+	last map[string]signal
+}
+
+func (m *conjunction) process(s signal, src string) (signal, bool) {
+	m.last[src] = s
+	for _, v := range m.last {
+		if v == low {
+			return high, true
+		}
+	}
+	return low, true
+}
+
+func (m *broadcaster) outs() []string { return m.out }
+func (m *flipflop) outs() []string    { return m.out }
+func (m *conjunction) outs() []string { return m.out }
+
 func main() {
 	lines := exp.MustReadLines(inputPath)
-	sum := 0
+	byName := make(map[string]module)
 	for _, line := range lines {
-		sum += exp.MustAtoi(line)
+		in, out, ok := strings.Cut(line, " -> ")
+		if !ok {
+			panic("invalid line " + line)
+		}
+		outs := strings.Split(out, ", ")
+		var m module
+		var name string
+		switch {
+		case in[0] == '%':
+			name = in[1:]
+			m = &flipflop{
+				out:   outs,
+				state: false,
+			}
+
+		case in[0] == '&':
+			name = in[1:]
+			m = &conjunction{
+				out:  outs,
+				last: make(map[string]signal),
+			}
+
+		case in == "broadcaster":
+			name = in
+			m = &broadcaster{
+				out: outs,
+			}
+
+		default:
+			panic(fmt.Sprintf("invalid name %q", name))
+		}
+
+		//fmt.Printf("name %q, module %v\n", name, m)
+		byName[name] = m
 	}
-	fmt.Println(sum)
+
+	// conjunctions initially remember low for all inputs
+	for name, mod := range byName {
+		for _, out := range mod.outs() {
+			dmod := byName[out]
+			if conj, ok := dmod.(*conjunction); ok {
+				conj.last[name] = low
+			}
+		}
+	}
+
+	type pulse struct {
+		src, dst string
+		sig      signal
+	}
+	highs, lows := 0, 0
+	for i := 0; i < 1000; i++ {
+		q := []pulse{{src: "button", dst: "broadcaster", sig: low}}
+		for len(q) > 0 {
+			p := q[0]
+			q = q[1:]
+			if p.sig == high {
+				highs++
+			} else {
+				lows++
+			}
+			dmod := byName[p.dst]
+			if dmod == nil {
+				//panic("no module named " + p.dst)
+				continue
+			}
+			ns, ok := dmod.process(p.sig, p.src)
+			if !ok {
+				continue
+			}
+			for _, out := range dmod.outs() {
+				q = append(q, pulse{
+					src: p.dst,
+					dst: out,
+					sig: ns,
+				})
+			}
+		}
+	}
+
+	fmt.Println(highs * lows)
 }
--- 2023/20b/main.go
+++ 2023/20b/main.go
@@ -2,8 +2,10 @@ package main
 
 import (
 	"fmt"
+	"strings"
 
 	"github.com/DrJosh9000/exp"
+	"github.com/DrJosh9000/exp/algo"
 )
 
 // Advent of Code 2023
@@ -11,11 +13,100 @@ import (
 
 const inputPath = "2023/inputs/20.txt"
 
+type module interface {
+	outs() []string
+}
+
+type broadcaster struct {
+	out []string
+}
+
+type flipflop struct {
+	out []string
+}
+
+type conjunction struct {
+	out []string
+}
+
+func (m *broadcaster) outs() []string { return m.out }
+func (m *flipflop) outs() []string    { return m.out }
+func (m *conjunction) outs() []string { return m.out }
+
 func main() {
 	lines := exp.MustReadLines(inputPath)
-	sum := 0
+	byName := make(map[string]module)
 	for _, line := range lines {
-		sum += exp.MustAtoi(line)
+		in, out, ok := strings.Cut(line, " -> ")
+		if !ok {
+			panic("invalid line " + line)
+		}
+		outs := strings.Split(out, ", ")
+		var m module
+		var name string
+		switch {
+		case in[0] == '%':
+			name = in[1:]
+			m = &flipflop{
+				out: outs,
+			}
+
+		case in[0] == '&':
+			name = in[1:]
+			m = &conjunction{
+				out: outs,
+			}
+
+		case in == "broadcaster":
+			name = in
+			m = &broadcaster{
+				out: outs,
+			}
+
+		default:
+			panic(fmt.Sprintf("invalid name %q", name))
+		}
+
+		byName[name] = m
 	}
-	fmt.Println(sum)
+
+	var highs []int
+
+	// (some scribbling on paper)
+	// these flipflops and conjunctions are wired suspiciously like four
+	// pulse counters
+
+	for _, mod := range byName["broadcaster"].outs() {
+		ff, ok := byName[mod].(*flipflop)
+		if !ok {
+			panic("not a flipflop")
+		}
+		n, b := 0, 1
+		chain := true
+		for chain {
+			chain = false
+			for _, ffo := range ff.outs() {
+				switch next := byName[ffo].(type) {
+				case *conjunction:
+					n += b
+				case *flipflop:
+					ff = next
+					chain = true
+				}
+			}
+			b *= 2
+		}
+		highs = append(highs, n)
+	}
+
+	//fmt.Println(highs)
+
+	n := 1
+	for _, h := range highs {
+		d := algo.GCD(n, h)
+		n /= d
+		n *= h
+	}
+
+	fmt.Println(n)
 }
