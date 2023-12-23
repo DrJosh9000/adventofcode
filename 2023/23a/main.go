6ccae8f7d6cea0bee5d7288082ee44d1982c5fe9
Josh Deprez <josh.deprez@gmail.com>
2023-12-23 17:58:12 +1100

    2023/23

--- 2023/23a/main.go
+++ 2023/23a/main.go
@@ -2,8 +2,11 @@ package main
 
 import (
 	"fmt"
+	"image"
 
 	"github.com/DrJosh9000/exp"
+	"github.com/DrJosh9000/exp/algo"
+	"github.com/DrJosh9000/exp/grid"
 )
 
 // Advent of Code 2023
@@ -12,10 +15,77 @@ import (
 const inputPath = "2023/inputs/23.txt"
 
 func main() {
-	lines := exp.MustReadLines(inputPath)
-	sum := 0
-	for _, line := range lines {
-		sum += exp.MustAtoi(line)
+	g := exp.MustReadByteGrid(inputPath)
+	bounds := g.Bounds()
+	h, w := g.Size()
+
+	var start, goal image.Point
+	for x, c := range g[0] {
+		if c == '.' {
+			start.X = x
+		}
 	}
-	fmt.Println(sum)
+	goal.Y = h - 1
+	for x, c := range g[goal.Y] {
+		if c == '.' {
+			goal.X = x
+		}
+	}
+
+	been := grid.Make[bool](h, w)
+	longest := 0
+	var search func([]image.Point)
+	search = func(path []image.Point) {
+		n1 := len(path) - 1
+		p := path[n1]
+		if p == goal {
+			longest = max(longest, n1)
+			// fmt.Println(been)
+			return
+		}
+
+		var q image.Point
+		switch g.At(p) {
+		case '>':
+			q = p.Add(image.Pt(1, 0))
+
+		case '<':
+			q = p.Add(image.Pt(-1, 0))
+
+		case '^':
+			q = p.Add(image.Pt(0, -1))
+
+		case 'v':
+			q = p.Add(image.Pt(0, 1))
+
+		case '.':
+			for _, d := range algo.Neigh4 {
+				q := p.Add(d)
+				if !q.In(bounds) {
+					continue
+				}
+				if g.At(q) == '#' {
+					continue
+				}
+				if been.At(q) {
+					continue
+				}
+				been[q.Y][q.X] = true
+				search(append(path, q))
+				been[q.Y][q.X] = false
+			}
+			return
+		}
+
+		if been.At(q) {
+			return
+		}
+		been[q.Y][q.X] = true
+		search(append(path, q))
+		been[q.Y][q.X] = false
+	}
+
+	search([]image.Point{start})
+
+	fmt.Println(longest)
 }
--- 2023/23b/main.go
+++ 2023/23b/main.go
@@ -2,8 +2,11 @@ package main
 
 import (
 	"fmt"
+	"image"
 
 	"github.com/DrJosh9000/exp"
+	"github.com/DrJosh9000/exp/algo"
+	"github.com/DrJosh9000/exp/grid"
 )
 
 // Advent of Code 2023
@@ -11,11 +14,58 @@ import (
 
 const inputPath = "2023/inputs/23.txt"
 
+// Warning:
+// "${solution}"  482.28s user 60.83s system 124% cpu 7:15.39 total
+
 func main() {
-	lines := exp.MustReadLines(inputPath)
-	sum := 0
-	for _, line := range lines {
-		sum += exp.MustAtoi(line)
+	g := exp.MustReadByteGrid(inputPath)
+	bounds := g.Bounds()
+	h, w := g.Size()
+
+	var start, goal image.Point
+	for x, c := range g[0] {
+		if c == '.' {
+			start.X = x
+		}
 	}
-	fmt.Println(sum)
+	goal.Y = h - 1
+	for x, c := range g[goal.Y] {
+		if c == '.' {
+			goal.X = x
+		}
+	}
+
+	been := grid.Make[bool](h, w)
+	longest := 0
+	var search func([]image.Point)
+	search = func(path []image.Point) {
+		n1 := len(path) - 1
+		p := path[n1]
+		if p == goal {
+			longest = max(longest, n1)
+			// fmt.Println(been)
+			return
+		}
+
+		for _, d := range algo.Neigh4 {
+			q := p.Add(d)
+			if !q.In(bounds) {
+				continue
+			}
+			if g.At(q) == '#' {
+				continue
+			}
+			if been.At(q) {
+				continue
+			}
+			been[q.Y][q.X] = true
+			search(append(path, q))
+			been[q.Y][q.X] = false
+		}
+
+	}
+
+	search([]image.Point{start})
+
+	fmt.Println(longest)
 }
