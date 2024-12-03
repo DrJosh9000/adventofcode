5fbb154d84c576d87f252243242de2322ecd39ae
Josh Deprez <josh.deprez@gmail.com>
2024-12-03 20:40:13 +1100

    2024/3

---
+++ 2024/3a.go
@@ -0,0 +1,21 @@
+package main
+
+import (
+	_ "embed"
+	"fmt"
+	"regexp"
+
+	"github.com/DrJosh9000/exp"
+)
+
+//go:embed inputs/3.txt
+var input string
+
+func main() {
+	mulRE := regexp.MustCompile(`mul\((\d+),(\d+)\)`)
+	sum := 0
+	for _, sm := range mulRE.FindAllStringSubmatch(input, -1) {
+		sum += exp.MustAtoi(sm[1]) * exp.MustAtoi(sm[2])
+	}
+	fmt.Println(sum)
+}
---
+++ 2024/3b.go
@@ -0,0 +1,45 @@
+package main
+
+import (
+	_ "embed"
+	"fmt"
+	"regexp"
+	"strings"
+
+	"github.com/DrJosh9000/exp"
+)
+
+//go:embed inputs/3.txt
+var input string
+
+func main() {
+	opRE := regexp.MustCompile(`(do|don't|mul)\(([\d,]*)\)`)
+	sum := 0
+	enable := true
+	for _, sm := range opRE.FindAllStringSubmatch(input, -1) {
+		switch sm[1] {
+		case "mul":
+			if !enable {
+				continue
+			}
+			args := strings.Split(sm[2], ",")
+			if len(args) != 2 {
+				continue
+			}
+			sum += exp.MustAtoi(args[0]) * exp.MustAtoi(args[1])
+
+		case "do":
+			if sm[2] != "" {
+				continue
+			}
+			enable = true
+
+		case "don't":
+			if sm[2] != "" {
+				continue
+			}
+			enable = false
+		}
+	}
+	fmt.Println(sum)
+}
