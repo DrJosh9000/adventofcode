8e70b72833a583468d3ca87a34bfb3d26f630e2e
Josh Deprez <josh.deprez@gmail.com>
2024-12-05 09:41:37 +1100

    2024/4

---
+++ 2024/4a.go
@@ -0,0 +1,44 @@
+package main
+
+import (
+	_ "embed"
+	"fmt"
+	"image"
+	"strings"
+
+	"github.com/DrJosh9000/exp/algo"
+	"github.com/DrJosh9000/exp/grid"
+)
+
+//go:embed inputs/4.txt
+var input string
+
+func main() {
+	G := grid.BytesFromStrings(strings.Split(input, "\n"))
+	bounds := G.Bounds()
+	count := 0
+	for y, row := range G {
+		for x, chr := range row {
+			if chr != 'X' {
+				continue
+			}
+			p := image.Pt(x, y)
+		dirLoop:
+			for _, delta := range algo.Neigh8 {
+				q := p
+				str := make([]byte, 0, 3)
+				for range 3 {
+					q = q.Add(delta)
+					if !q.In(bounds) {
+						continue dirLoop
+					}
+					str = append(str, G.At(q))
+				}
+				if string(str) == "MAS" {
+					count++
+				}
+			}
+		}
+	}
+	fmt.Println(count)
+}
---
+++ 2024/4b.go
@@ -0,0 +1,42 @@
+package main
+
+import (
+	_ "embed"
+	"fmt"
+	"image"
+	"strings"
+
+	"github.com/DrJosh9000/exp/algo"
+	"github.com/DrJosh9000/exp/grid"
+)
+
+//go:embed inputs/4.txt
+var input string
+
+func main() {
+	G := grid.BytesFromStrings(strings.Split(input, "\n"))
+	bounds := G.Bounds()
+	count := 0
+	for y, row := range G {
+		for x, chr := range row {
+			if chr != 'A' {
+				continue
+			}
+			p := image.Pt(x, y)
+		dirLoop:
+			for _, dir := range algo.Neigh4 {
+				b0, b1 := p.Sub(dir), p.Add(dir)
+				r := image.Pt(-dir.Y, dir.X)
+				for _, q := range []image.Point{b0.Add(r), b0.Sub(r), b1.Add(r), b1.Sub(r)} {
+					if !q.In(bounds) {
+						break dirLoop
+					}
+				}
+				if G.At(b0.Add(r)) == 'M' && G.At(b0.Sub(r)) == 'M' && G.At(b1.Add(r)) == 'S' && G.At(b1.Sub(r)) == 'S' {
+					count++
+				}
+			}
+		}
+	}
+	fmt.Println(count)
+}
