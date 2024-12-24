4ae778b6f355f585fe975b13ce1a724a23f27374
Josh Deprez <josh.deprez@gmail.com>
2024-12-24 20:26:31 +1100

    2024/20

--- 2024/20a/main.go
+++ 2024/20a/main.go
@@ -2,8 +2,12 @@ package main
 
 import (
 	"fmt"
+	"image"
+	"iter"
 
 	"drjosh.dev/exp"
+	"drjosh.dev/exp/algo"
+	"drjosh.dev/exp/grid"
 )
 
 // Advent of Code 2024
@@ -12,10 +16,53 @@ import (
 const inputPath = "2024/inputs/20.txt"
 
 func main() {
-	lines := exp.MustReadLines(inputPath)
-	sum := 0
-	for _, line := range lines {
-		sum += exp.MustAtoi(line)
+	G := exp.MustReadByteGrid(inputPath)
+	bounds := G.Bounds()
+	var start, end image.Point
+	for p, c := range G.All {
+		switch c {
+		case 'S':
+			start = p
+		case 'E':
+			end = p
+		}
 	}
-	fmt.Println(sum)
+
+	D := grid.Make[int](G.Size())
+	prev, _ := algo.FloodFill(start, func(p image.Point, d int) (iter.Seq[image.Point], error) {
+		return func(yield func(p image.Point) bool) {
+			D.Set(p, d)
+			for _, d := range algo.Neigh4 {
+				if q := p.Add(d); q.In(bounds) && G.At(q) != '#' {
+					if !yield(q) {
+						return
+					}
+				}
+			}
+		}, nil
+	})
+
+	dia := []image.Point{
+		{-2, 0}, {-1, -1}, {0, -2}, {1, -1}, {2, 0}, {1, 1}, {0, 2}, {-1, 1},
+	}
+
+	cheats := make(algo.Set[cheat])
+	for p := end; len(prev[p]) > 0; p = prev[p][0] {
+		for _, d := range dia {
+			q := p.Add(d)
+			if q.In(bounds) && G.At(q) != '#' && D.At(p)-D.At(q) >= 102 {
+				cheats.Insert(order(p, q))
+			}
+		}
+	}
+	fmt.Println(len(cheats))
+}
+
+type cheat struct{ s, e image.Point }
+
+func order(p, q image.Point) cheat {
+	if p.X < q.X || (p.X == q.X && p.Y < q.Y) {
+		return cheat{p, q}
+	}
+	return cheat{q, p}
 }
--- 2024/20b/main.go
+++ 2024/20b/main.go
@@ -2,8 +2,12 @@ package main
 
 import (
 	"fmt"
+	"image"
+	"iter"
 
 	"drjosh.dev/exp"
+	"drjosh.dev/exp/algo"
+	"drjosh.dev/exp/grid"
 )
 
 // Advent of Code 2024
@@ -12,10 +16,60 @@ import (
 const inputPath = "2024/inputs/20.txt"
 
 func main() {
-	lines := exp.MustReadLines(inputPath)
-	sum := 0
-	for _, line := range lines {
-		sum += exp.MustAtoi(line)
+	G := exp.MustReadByteGrid(inputPath)
+	bounds := G.Bounds()
+	var start, end image.Point
+	for p, c := range G.All {
+		switch c {
+		case 'S':
+			start = p
+		case 'E':
+			end = p
+		}
 	}
-	fmt.Println(sum)
+
+	D := grid.Make[int](G.Size())
+	prev, _ := algo.FloodFill(start, func(p image.Point, d int) (iter.Seq[image.Point], error) {
+		return func(yield func(p image.Point) bool) {
+			D.Set(p, d)
+			for _, d := range algo.Neigh4 {
+				if q := p.Add(d); q.In(bounds) && G.At(q) != '#' {
+					if !yield(q) {
+						return
+					}
+				}
+			}
+		}, nil
+	})
+
+	cheats := make(algo.Set[cheat])
+	var path []image.Point
+	for p := end; ; p = prev[p][0] {
+		path = append(path, p)
+		if p == start {
+			break
+		}
+	}
+
+	for p, q := range algo.AllPairs(path) {
+		d := p.Sub(q)
+		m := algo.L1(d)
+		if m > 20 {
+			continue
+		}
+		if algo.Abs(D.At(p)-D.At(q)) >= 100+m {
+			cheats.Insert(order(p, q))
+		}
+	}
+
+	fmt.Println(len(cheats))
+}
+
+type cheat struct{ s, e image.Point }
+
+func order(p, q image.Point) cheat {
+	if p.X < q.X || (p.X == q.X && p.Y < q.Y) {
+		return cheat{p, q}
+	}
+	return cheat{q, p}
 }
