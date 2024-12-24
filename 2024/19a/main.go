297f57f9133a77981b00b2be80875ce9e1395755
Josh Deprez <josh.deprez@gmail.com>
2024-12-24 17:53:31 +1100

    2024/19

--- 2024/19a/main.go
+++ 2024/19a/main.go
@@ -2,6 +2,8 @@ package main
 
 import (
 	"fmt"
+	"regexp"
+	"strings"
 
 	"drjosh.dev/exp"
 )
@@ -13,9 +15,13 @@ const inputPath = "2024/inputs/19.txt"
 
 func main() {
 	lines := exp.MustReadLines(inputPath)
-	sum := 0
-	for _, line := range lines {
-		sum += exp.MustAtoi(line)
+	types := strings.Split(lines[0], ", ")
+	stripesRE := regexp.MustCompile("^(" + strings.Join(types, "|") + ")+$")
+	count := 0
+	for _, towel := range lines[2:] {
+		if stripesRE.MatchString(towel) {
+			count++
+		}
 	}
-	fmt.Println(sum)
+	fmt.Println(count)
 }
--- 2024/19b/main.go
+++ 2024/19b/main.go
@@ -2,6 +2,8 @@ package main
 
 import (
 	"fmt"
+	"regexp"
+	"strings"
 
 	"drjosh.dev/exp"
 )
@@ -13,9 +15,31 @@ const inputPath = "2024/inputs/19.txt"
 
 func main() {
 	lines := exp.MustReadLines(inputPath)
-	sum := 0
-	for _, line := range lines {
-		sum += exp.MustAtoi(line)
+	types := strings.Split(lines[0], ", ")
+	stripesRE := regexp.MustCompile("^(" + strings.Join(types, "|") + ")+$")
+	count := 0
+	for _, towel := range lines[2:] {
+		if !stripesRE.MatchString(towel) {
+			continue
+		}
+		memo := map[string]int{"": 1}
+		var search func(string) int
+		search = func(towel string) int {
+			if c, has := memo[towel]; has {
+				return c
+			}
+			c := 0
+			for _, t := range types {
+				rem, ok := strings.CutPrefix(towel, t)
+				if !ok {
+					continue
+				}
+				c += search(rem)
+			}
+			memo[towel] = c
+			return c
+		}
+		count += search(towel)
 	}
-	fmt.Println(sum)
+	fmt.Println(count)
 }
