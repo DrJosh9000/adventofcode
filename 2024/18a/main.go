95f5443c2290d4e7c94aabd075ae1963aece8217
Josh Deprez <josh.deprez@gmail.com>
2024-12-24 17:34:09 +1100

    2024/18

--- 2024/18a/main.go
+++ 2024/18a/main.go
@@ -2,8 +2,13 @@ package main
 
 import (
 	"fmt"
+	"image"
+	"iter"
+	"math"
 
 	"drjosh.dev/exp"
+	"drjosh.dev/exp/algo"
+	"drjosh.dev/exp/grid"
 )
 
 // Advent of Code 2024
@@ -13,9 +18,46 @@ const inputPath = "2024/inputs/18.txt"
 
 func main() {
 	lines := exp.MustReadLines(inputPath)
-	sum := 0
-	for _, line := range lines {
-		sum += exp.MustAtoi(line)
+	obs := make(algo.Set[image.Point])
+	for _, line := range lines[:1024] {
+		var p image.Point
+		fmt.Sscanf(line, "%d,%d", &p.X, &p.Y)
+		obs.Insert(p)
 	}
-	fmt.Println(sum)
+
+	bounds := image.Rect(0, 0, 71, 71)
+	best := math.MaxInt
+	end := image.Pt(70, 70)
+	pred, _ := algo.AStar(
+		image.Pt(0, 0),
+		func(p image.Point) int { return algo.L1(p.Sub(end)) },
+		func(p image.Point, d int) (iter.Seq2[image.Point, int], error) {
+			if p == end {
+				best = min(best, d)
+				return nil, nil
+			}
+			return func(yield func(image.Point, int) bool) {
+				for _, e := range algo.Neigh4 {
+					q := p.Add(e)
+					if q.In(bounds) && !obs.Contains(q) {
+						if !yield(q, 1) {
+							return
+						}
+					}
+				}
+			}, nil
+		},
+	)
+
+	G := grid.Make[byte](71, 71)
+	G.Fill('.')
+	for p := range obs {
+		G.Set(p, '#')
+	}
+	for p := end; p != image.Pt(0, 0); p = pred[p][0] {
+		G.Set(p, 'O')
+	}
+	fmt.Println(G)
+
+	fmt.Println(best)
 }
--- 2024/18b/main.go
+++ 2024/18b/main.go
@@ -2,8 +2,12 @@ package main
 
 import (
 	"fmt"
+	"image"
+	"iter"
+	"math"
 
 	"drjosh.dev/exp"
+	"drjosh.dev/exp/algo"
 )
 
 // Advent of Code 2024
@@ -13,9 +17,39 @@ const inputPath = "2024/inputs/18.txt"
 
 func main() {
 	lines := exp.MustReadLines(inputPath)
-	sum := 0
+	obs := make(algo.Set[image.Point])
+	bounds := image.Rect(0, 0, 71, 71)
+	end := image.Pt(70, 70)
 	for _, line := range lines {
-		sum += exp.MustAtoi(line)
+		var p image.Point
+		fmt.Sscanf(line, "%d,%d", &p.X, &p.Y)
+		obs.Insert(p)
+
+		best := math.MaxInt
+		pred, _ := algo.AStar(
+			image.Pt(0, 0),
+			func(p image.Point) int { return algo.L1(p.Sub(end)) },
+			func(p image.Point, d int) (iter.Seq2[image.Point, int], error) {
+				if p == end {
+					best = min(best, d)
+					return nil, nil
+				}
+				return func(yield func(image.Point, int) bool) {
+					for _, e := range algo.Neigh4 {
+						q := p.Add(e)
+						if q.In(bounds) && !obs.Contains(q) {
+							if !yield(q, 1) {
+								return
+							}
+						}
+					}
+				}, nil
+			},
+		)
+		if len(pred[end]) == 0 {
+			fmt.Println(p)
+			return
+		}
 	}
-	fmt.Println(sum)
+
 }
