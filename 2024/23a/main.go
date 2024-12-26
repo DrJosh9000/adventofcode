9b4f3eb1b47a2fb1ee58f94cba6374fda3efe2c1
Josh Deprez <josh.deprez@gmail.com>
2024-12-27 10:25:12 +1100

    2024/23

--- 2024/23a/main.go
+++ 2024/23a/main.go
@@ -2,8 +2,10 @@ package main
 
 import (
 	"fmt"
+	"slices"
 
 	"drjosh.dev/exp"
+	"drjosh.dev/exp/algo"
 )
 
 // Advent of Code 2024
@@ -13,9 +15,31 @@ const inputPath = "2024/inputs/23.txt"
 
 func main() {
 	lines := exp.MustReadLines(inputPath)
-	sum := 0
+	sets := make(map[string]algo.Set[string])
 	for _, line := range lines {
-		sum += exp.MustAtoi(line)
+		u, v := exp.MustCut(line, "-")
+		sets[u] = sets[u].Insert(v)
+		sets[v] = sets[v].Insert(u)
 	}
-	fmt.Println(sum)
+	triples := make(algo.Set[[3]string])
+	for u, ua := range sets {
+		for v := range ua {
+			va := sets[v]
+			for w := range va {
+				if u == w || v == w {
+					continue
+				}
+				if u[0] != 't' && v[0] != 't' && w[0] != 't' {
+					continue
+				}
+				if !sets[u].Contains(w) {
+					continue
+				}
+				trip := [3]string{u, v, w}
+				slices.Sort(trip[:])
+				triples.Insert(trip)
+			}
+		}
+	}
+	fmt.Println(len(triples))
 }
--- 2024/23b/main.go
+++ 2024/23b/main.go
@@ -2,8 +2,11 @@ package main
 
 import (
 	"fmt"
+	"slices"
+	"strings"
 
 	"drjosh.dev/exp"
+	"drjosh.dev/exp/algo"
 )
 
 // Advent of Code 2024
@@ -13,9 +16,43 @@ const inputPath = "2024/inputs/23.txt"
 
 func main() {
 	lines := exp.MustReadLines(inputPath)
-	sum := 0
+	sets := make(map[string]algo.Set[string])
 	for _, line := range lines {
-		sum += exp.MustAtoi(line)
+		u, v := exp.MustCut(line, "-")
+		sets[u] = sets[u].Insert(v)
+		sets[v] = sets[v].Insert(u)
 	}
-	fmt.Println(sum)
+
+	// largest clique is NP hard, right?
+	cliques := make(algo.Set[string])
+	for u, ua := range sets {
+		for v := range ua {
+			cl := []string{u, v}
+			slices.Sort(cl)
+			cliques.Insert(strings.Join(cl, ","))
+		}
+	}
+
+	for u, ua := range sets {
+		for cl := range cliques {
+			cv := strings.Split(cl, ",")
+			if slices.Contains(cv, u) {
+				continue
+			}
+			if ua.ContainsAll(cv...) {
+				cv2 := append(slices.Clone(cv), u)
+				slices.Sort(cv2)
+				cliques.Insert(strings.Join(cv2, ","))
+			}
+		}
+	}
+
+	largest := ""
+	for cl := range cliques {
+		if len(cl) > len(largest) {
+			largest = cl
+		}
+	}
+
+	fmt.Println(largest)
 }
