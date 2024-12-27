1465d121a92441fa6c2dd143b70774c7acd8ce27
Josh Deprez <josh.deprez@gmail.com>
2024-12-27 16:57:59 +1100

    2024/25

--- 2024/25/main.go
+++ 2024/25/main.go
@@ -13,9 +13,42 @@ const inputPath = "2024/inputs/25.txt"
 
 func main() {
 	lines := exp.MustReadLines(inputPath)
-	sum := 0
-	for _, line := range lines {
-		sum += exp.MustAtoi(line)
+	var locks, keys [][5]int
+lineLoop:
+	for i := 0; i < len(lines); i += 8 {
+		var hs [5]int
+		for j := range 5 {
+			if i+j+1 >= len(lines) {
+				break lineLoop
+			}
+			row := lines[i+j+1]
+			for k, c := range row {
+				if c == '#' {
+					hs[k]++
+				}
+			}
+		}
+		if lines[i] == "#####" {
+			locks = append(locks, hs)
+		} else {
+			keys = append(keys, hs)
+		}
 	}
-	fmt.Println(sum)
+	count := 0
+	for _, l := range locks {
+		for _, k := range keys {
+			overlap := false
+			for i := range 5 {
+				if l[i]+k[i] > 5 {
+					overlap = true
+					break
+				}
+			}
+			if !overlap {
+				count++
+			}
+		}
+	}
+
+	fmt.Println(count)
 }
